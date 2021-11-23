c4240da2cf37b82117468fde4ca6249027444735
Ewooral <boahen.cos@gmail.com>
2021-11-23 01:20:31 +0000

    tutorials on tuple and dictionary

---
+++ Intermediate Python Tutorials/funtions.py
@@ -0,0 +1,17 @@
+def increment_dictionary_values(d, i):
+    
+    for k, v in d.items():
+        d[k] = v + i 
+    return d
+from unittest import TestCase
+class TestIncrementDictionaryValues(TestCase):
+     def test_increment_dictionary_values(self):
+         d = {'a': 1}
+         dd = increment_dictionary_values(d, 1)
+         ddd = increment_dictionary_values(d, -1)
+         self.assertEqual(dd['a'], 2)
+         self.assertEqual(ddd['a'], 0)
+
+
+assert sum([1, 2, 1]) == 6
+
