7fd3e332707d46e52e82ac424599f4196bef5824
Ewooral <boahen.cos@gmail.com>
2021-12-09 01:49:47 +0000

    requre function in node JS

--- NodeJS/nodetutorials/globalObject.js
+++ NodeJS/nodetutorials/globalObject.js
@@ -1,18 +1,21 @@
-// let sayHello = "Hello";
-// console.log(sayHello);
+let sayHello = "Hello";
+console.log(sayHello);
 global.console.log('sayHello'); 
 
-// console.log(__dirname);
-// console.log(__filename);
-// console.log(process);
-// console.log(require);
+console.log(__dirname);
+console.log(__filename);
+console.log(process);
+console.log(require);
 
-// const path = require('path');
-// console.log(`The file name is ${path.basename(__filename)}`);
+const path = require('path');
+console.log(`The file name is ${path.basename(__filename)}`);
 
-// console.log(process.pid);
-// console.log(process.versions);
-// console.dir(process)
-// console.log('Here you are: ' + JSON.stringify(process.env));
-// console.log(process.argv);
+console.log(process.pid);
+console.log(process.versions);
+console.dir(process)
+console.log('Here you are: ' + JSON.stringify(process.env));
+console.log(process.argv);
  
+clg = console.log;
+clg('me')
+clg('you') 
