0b7ec273d9e79a0476ad29d9cf10c3c4a28d0d63
Ewooral <boahen.cos@gmail.com>
2021-12-21 17:44:40 +0000

    advanced function constructor function

--- Javascript Intermediate Tutorials/arrayOfObject.js
+++ Javascript Intermediate Tutorials/arrayOfObject.js
@@ -10,7 +10,7 @@ const users = [
     { name:'Joe Smith', state: 'ID'},
 
 ];
-
+ 
 // A function that doubles numbers in an array
 const numb = [1,2, 3, 4, 5,4,5];
 
@@ -24,12 +24,41 @@ const doubledNumb = (number) => {
 }
 print(doubledNumb(numb));
 
-
+// A function that multipliers given parameters
 function multiplier(x =3, y=5){
     x = x || 4 // x equals to x or 4. if x is not given a value, it will use the default value
-    y = y || 2
+    y = y || 2 
     return x * y;
 }
 
 print(multiplier(3))
 
+
+// function multiply(multiplier, ...args) {
+// // console.log(arguments)  // arguments is a built-in function that converts the arguments into codes.
+// //  const args = Array.prototype.slice.call(arguments, logAllArguments.length)
+// // print(args.sort())
+// return args.map(function(x){
+//     multiplier * x
+// })
+// }
+// print(multiply(Number ,Boolean, 1, true, 2, 3, false, 7, 4, 5)
+// )
+
+function multiply(multiplier, ...nums) {
+    return nums.map((x) =>  multiplier * x)
+}
+
+print(multiply(4,2,3,4,5, false))
+
+// Function Constructor
+function Dog(name, height) {
+    this.puppy = name;
+    this.stature = height;
+    setInterval(()=>{
+        this.stature+=1;
+      print(this)
+    }, 1000)
+}
+const Dog1 = new Dog('hush Puppy', 0);
+// print(Dog1);
