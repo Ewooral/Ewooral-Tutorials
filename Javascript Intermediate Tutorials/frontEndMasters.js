25ea844bbb90f94bbbc6981672102956e9811175
Ewooral <boahen.cos@gmail.com>
2021-12-25 04:02:47 +0000

    coding done on the 25th of December 2021

--- Javascript Intermediate Tutorials/frontEndMasters.js
+++ Javascript Intermediate Tutorials/frontEndMasters.js
@@ -25,3 +25,36 @@ addFavoriteBook("Intro to Algorithms")
 addFavoriteBook("Greater things ahead")
 
 printFavoriteBooks()
+
+var show = console.log
+var num = 6;
+var letter = `whoami`;
+var anothernum = 6;
+
+if(
+  (num === letter || num === anothernum) &&
+  (letter === "whoami" || `whoami`)  
+){
+  console.log("Yes!")
+}else{
+  "Laugh!"
+}
+
+function ask(question){
+  setTimeout(function  waitASec(){
+    show(question);
+  }, 900);
+}
+ask("What is your name  ")
+
+
+function ask(question){
+  console.log(this.teacher, question)
+}
+function otherClass(){
+  var myContext = {
+    teacher:"Suzy"
+  };
+  ask.call(myContext, "why?")
+}
+otherClass();
