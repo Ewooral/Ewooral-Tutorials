97d4bb61145290c334725f50d00850b5b57cf979
Ewooral <boahen.cos@gmail.com>
2022-01-10 18:57:29 +0000

    c++ numbers

---
+++ C++/numbers.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    cout << 2 + 3 << endl;
+    cout << 2 - 3 << endl;
+    cout << 2 * 3 << endl;
+    cout << 2.0 / 3 << endl;
+    cout << 2 % 3 << endl;
+
+    cout << ".......NUMBER INBUILT FUNCTIONS........" << endl;
+    cout << pow(2, 3) << endl;  // 2 raised to the power 
+    cout << sqrt(9) << endl;    // square root
+    cout << abs(-3) << endl;    // absolute value
+    cout << ceil(3.2) << endl;  // round up
+    cout << floor(3.2) << endl; // round down
+    cout << round(3.2) << endl; // round to nearest integer
+    cout << trunc(3.2) << endl; // round down to integer
+    cout << rand() << endl;     // random number
+    cout << max(2, 3) << endl;  // max of 2 and 3
+    cout << min(2, 3) << endl;  // min of 2 and 3
+
+}
