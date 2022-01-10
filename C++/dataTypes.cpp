1ba286c42ea7cb152761d380803466dc67244238
Ewooral <boahen.cos@gmail.com>
2022-01-10 17:44:18 +0000

    c++ datatypes

---
+++ C++/dataTypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char aCharacter = 'A';
+    cout << aCharacter << endl;
+    int age = 50;
+    cout << age << endl;
+    float weight = 150.5;
+    cout << weight << endl;
+    double height = 1.75;
+    cout << height << endl;
+    bool isValid = true;
+    cout << isValid << endl;
+    string myFirstName = "Elijah";
+    cout << myFirstName << endl;
+}
