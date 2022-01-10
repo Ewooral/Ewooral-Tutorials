ba152181ec41e3d93a3ead181275609741d470f2
Ewooral <boahen.cos@gmail.com>
2022-01-10 19:26:25 +0000

    getting user inputs

---
+++ C++/userInput.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int age;
+    string name;
+    
+    // cout << "Enter your age: ";
+    cout << "Enter your name: ";
+
+    // cin >> age;
+    getline(cin, name); // getline(variable, max length of the string))
+
+    // if we want to enter a string as an input then we use
+    //  getline(cin, name)
+    // cout << "You are " << age << " years old. " ;
+
+    cout << "Hello " << name << "." << endl;
+    return 0;
+}
