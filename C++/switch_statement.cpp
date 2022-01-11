c5052365cb4396b91775897fbb13f64cabf4de15
Ewooral <boahen.cos@gmail.com>
2022-01-11 19:44:09 +0000

    switch statement

--- C++/switch_statement.cpp
+++ C++/switch_statement.cpp
@@ -7,7 +7,7 @@ string getDayOfWeek(int dayNumber);
 // MAIN FUNCTION
 int main()
 {
-
+    cout << getDayOfWeek(5);
     return 0;
 }
 
@@ -17,7 +17,30 @@ string getDayOfWeek(int dayNumber)
     switch(dayNumber)
     {
         case 0: 
-            dayName = "Sunday"
+            dayName = "Sunday";
+            break;
+        case 1:
+            dayName = "Monday";
+            break;
+        case 2:
+            dayName = "Tuesday";
+            break;
+        case 3:
+            dayName = "Wednesday";
+            break;
+        case 4:
+            dayName = "Thursday";
+            break;
+        case 5:
+            dayName = "Friday";
+            break;
+        case 6:
+            dayName = "Saturday";
+            break;
+        default:
+            dayName = "Invalid Day of the Week";
+            break;
+            
     }
 
     return dayName;
