c29c10e7f61d728ec62a9d0e81df2de00d862feb
Ewooral <boahen.cos@gmail.com>
2022-01-11 17:37:40 +0000

    declaring a function without content before the main() function and defining its contents after the function

--- C++/function.cpp
+++ C++/function.cpp
@@ -1,26 +1,33 @@
 #include <iostream>
 using namespace std;
 
-void sayHi( string name)
-{
-  cout << "Hello, " << name << "!" << endl;
-}
-
-void greetEveryone() 
-{
-  string name;
-  cout << " who do you wanna greet? ";
-  getline(cin, name);
-  cout << "Hello, Good Morning " << name << "!" << endl;
 
 
-}
+// You can declare a function at the top of the main() function
+// and tell it what to do at anywhere in the program.
 
+void greetEveryone();
+void sayHi(string name, int age) ;
 
 
 int main()
 {
- sayHi("Elijah Owusu Ewooral Boahen");
+ sayHi("Elijah Owusu Ewooral Boahen", 29);
+ sayHi("Docker!", 30);
  greetEveryone();
  return 0;
+}
+
+void greetEveryone()
+{
+    string name;
+    cout << "Who do you wanna greet? ";
+    getline(cin, name);
+    cout << "Hello, Good Morning " << name << "!" << endl;
+}
+
+void sayHi(string name, int age)
+{
+    cout << "Hello, " << name << "!"
+         << " You are " << age << " years old." << endl;
 }
