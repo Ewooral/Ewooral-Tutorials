99c1ddce79312e75e1a4d550f045bdabbba5c282
Ewooral <boahen.cos@gmail.com>
2022-01-11 21:11:50 +0000

    for loop

---
+++ C++/for_loop.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int index[] = {1, 2, 3, 5, 12};
+    for( int i = 0; i < 5; i++)
+    {
+        cout << index[i] << endl;
+    }
+    return 0;
+}
