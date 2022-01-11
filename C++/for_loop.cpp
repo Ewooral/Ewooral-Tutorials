e6288bedb7ac1542e84a94edc2af0cf5b29c1c24
Ewooral <boahen.cos@gmail.com>
2022-01-11 21:52:41 +0000

    raise to the power function

--- C++/for_loop.cpp
+++ C++/for_loop.cpp
@@ -1,12 +1,19 @@
 #include <iostream>
+#include <cmath>
+
 using namespace std;
 
 int main()
 {
-    int index[] = {1, 2, 3, 5, 12};
-    for( int i = 0; i < 5; i++)
+    int index[8] = {1, 2, 3, 5, 12};
+        index[5] = 80;
+        index[6] = 300;
+        index[7] = 111;
+    for( int i = 0; i <= 7; i++)
     {
         cout << index[i] << endl;
     }
+    cout << "..............." << endl;
+    cout << pow(4, 2);
     return 0;
-}
+} 
---
+++ C++/raise_to_the_power.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int power(int baseNum, int powNum);
+
+int main()
+{
+    cout << power(3, 3) << endl;
+    
+    return 0;
+}
+
+int power(int baseNum, int powNum)
+{
+    cout << "The result of "<< baseNum
+         << " to the power " << powNum
+         << " is: " << endl;
+    int result = 1;
+    for(int i = 0; i < powNum; i++ )
+    {
+        result = result * baseNum;
+    }
+
+    return result;
+}
