4ef9856abc9bb3cd7a00bf9041e4fa589e03178b
Ewooral <boahen.cos@gmail.com>
2022-01-12 22:07:33 +0000

    two dimensional arrays

---
+++ C++/two_dimensional_array.cpp
@@ -0,0 +1,20 @@
+// Two dimensioanl arrays and
+// nested for loops
+// Two dimensional array is an array where 
+// every element in the array is an array
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int numberGrid[3][2] =
+    {
+        {1, 2},
+        {4, 5},
+        {7, 8}
+    };
+    cout << numberGrid[]
+
+    return 0;
+}
+ 
