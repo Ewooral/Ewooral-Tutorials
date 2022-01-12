9043ec5f78fb77f5661dedd55fb01c69666ee8d0
Ewooral <boahen.cos@gmail.com>
2022-01-12 04:32:22 +0000

    stack data structure

---
+++ ALGORITHMS/data structures/stack.js
@@ -0,0 +1,18 @@
+var stack = function (){
+    this.storage = " ";
+};
+
+stack.prototype.push = function(val){
+ this.push(val);
+};
+
+stack.prototype.pop = function(){
+
+};
+
+stack.prototype.size = function(){
+
+};
+
+var myWeeklyMenu = new stack();
+myWeeklyMenu.push("pizza");
