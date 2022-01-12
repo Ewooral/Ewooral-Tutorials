b7bd64c706b869c8a18315cfd3a6477aaf0e4ff6
Ewooral <boahen.cos@gmail.com>
2022-01-12 22:32:03 +0000

    pointers in c++

---
+++ C++/pointers.cpp
@@ -0,0 +1,12 @@
+// Pointers in c++ are memory addresses.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int number = 5;
+    double gpa = 3.5;
+    string name = "Elijah";
+
+    return 0;
+}
