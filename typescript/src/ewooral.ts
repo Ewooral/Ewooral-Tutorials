db09580ffa29deb3dd423e499c62c562039b2881
Ewooral <boahen.cos@gmail.com>
2022-01-13 14:54:55 +0000

    typescript variable and value

--- typescript/src/ewooral.ts
+++
@@ -1,6 +0,0 @@
-let Name: string = "Ewooral";
-Name = "Boahen"
-console.log(Name)
-
-const age = 29;
-console.log(age)
---
+++ typescript/src/variable_and_value.ts
@@ -0,0 +1,21 @@
+let Name: string = "Ewooral";
+Name = "Boahen"
+var age = 29; // const is read only. 29 is a literal type
+let startTime = new Date();
+let endTime =  Date
+
+
+while(age){
+   if(age <= 50)
+   {
+      age++
+      console.log(age)
+   }
+   if(age == 50)
+   {
+       console.log("Age is the same as 50")
+       break;
+   }
+}
+
+
