fda9ceba03b8a7a9bdfd8426fb64beec568b3b31
Ewooral <boahen.cos@gmail.com>
2022-01-13 00:31:57 +0000

    c++ enum examples and how to use it

--- C++/enum.cpp
+++ C++/enum.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
 using namespace std;
+
 /*In C++ programming, enum or enumeration is a data type 
 consisting of named values like elements, members, etc., 
 that represent integral constants. It provides a way to 
 define and group integral constants. It also makes the code
  easy to maintain and less complex. In this tutorial, you will
 learn about C++ enum in detail.*/
+
 enum color
 {
     red,
     green,
-    blue
+    blue,
+    yellow,
+    black,
+    white, 
+    orange
 };
 
 int main()
 {
 
-    color ewoo = red;
+    color ewoo = green;
     if (ewoo == green)
     {
         cout << "ewoo is green "<<"and its value is "<<ewoo << endl;
+       
+        int bitwiseValue = yellow | orange;
+        cout << bitwiseValue << endl;
     }
     else
     {
