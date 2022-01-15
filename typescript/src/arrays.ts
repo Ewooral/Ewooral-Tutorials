d3b79a4fdbfb83ebd53d7d9843b537029aba2a91
Ewooral <boahen.cos@gmail.com>
2022-01-15 16:07:16 +0000

    arrays in typescript

---
+++ typescript/src/arrays.ts
@@ -0,0 +1,26 @@
+//ARRAY
+// const Cars : [
+//      Cars:{
+//         make:string;
+//         model:string;
+//         year:number;
+//     }[]
+//     {
+//         make: 'Ford'
+//         model: 'Fiesta'
+//         year: 2018
+//     }
+// ]
+
+let arr :number[] = [1,2,3,4,5];
+let arrr: Array<number> = [4,5,6,7,2]; 
+console.log(arr[3]);
+console.log(arrr);
+console.log(typeof(arr));
+console.log(typeof(arrr));
+
+const numPair: [number, number] = [11, 12];
+numPair.forEach((x)=>{
+    console.log(x+20);
+})
+
--- typescript/src/variable_and_value.ts
+++ typescript/src/variable_and_value.ts
@@ -4,6 +4,8 @@ var age = 29; // const is read only. 29 is a literal type
 let startTime = new Date();
 let endTime =  Date
 
+// To learn more about the language, click above in "Examples" or "What's New".
+// Otherwise, get started by removing these comments and the world is your playground.
 
 while(age){
    if(age <= 50)
