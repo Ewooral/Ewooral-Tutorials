a0f32eb819433fca09655571d6e2af23c6d3250c
Ewooral <boahen.cos@gmail.com>
2022-01-16 02:02:12 +0000

    a typescript function that searches for a string in an array and returns it

--- typescript/src/functions.ts
+++ typescript/src/functions.ts
@@ -3,3 +3,15 @@ function add(a:number, b:number):number{
 }
 
 console.log( add(3, 5))
+
+// a function that takes in an array and searches for a particular element
+const nemo: string[] = ["Nemo"];
+
+function findNemo(array: string[]){
+    for(let i = 0; i < array.length; i++){
+        if(array[i] == "Nemo"){
+            console.log("Found Nemo");
+        }
+    }
+}
+findNemo(nemo);
