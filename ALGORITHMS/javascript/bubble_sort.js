8566e0e75a8a2d8496385d86b51ff7a48fa32b85
Ewooral <boahen.cos@gmail.com>
2022-01-20 02:26:22 +0000

    bubblesort algorithm using both do while and while loop

--- ALGORITHMS/javascript/bubble_sort.js
+++ ALGORITHMS/javascript/bubble_sort.js
@@ -15,4 +15,21 @@ function bubbleSort(nums){
     return nums;
 
 }
+
+function bubbleSorting(nums) {
+  let swapped = false;
+  while (swapped) {
+    for (let i = 0; i < nums.length; i++) {
+      if (nums[i] > nums[i] + 1) {
+        const temp = nums[i];
+        nums[i] = nums[i + 1];
+        nums[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  }
+
+  return nums;
+}
 console.log(bubbleSort([10,5,3,8,2,6,4,7,9,1]));
+console.log(bubbleSorting([20,5,3,18,2,6,4,17,9,11]));
