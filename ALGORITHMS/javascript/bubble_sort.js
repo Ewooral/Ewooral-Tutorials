2a16b0ea966e8c9c0efe4472b74fd9a0c5cbbd88
Ewooral <boahen.cos@gmail.com>
2022-01-20 00:09:52 +0000

    bubble sort algorithm

---
+++ ALGORITHMS/javascript/bubble_sort.js
@@ -0,0 +1,15 @@
+function bubbleSort(nums) {
+  // code goes here
+  let sortedArray = [];
+  while(nums){
+    for(let i=0; i < nums.length; i++){
+      if(nums[i] > nums[0]){
+        nums[0] = nums[i]
+      }
+    }
+
+  }
+
+
+  return sortedArray;
+}
