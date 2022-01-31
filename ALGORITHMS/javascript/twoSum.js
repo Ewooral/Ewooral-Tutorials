0274b5dc8931b3a62d879ab5c9580574b9c7b68a
Ewooral <boahen.cos@gmail.com>
2022-01-31 21:58:18 +0000

    time and space complexity of the two sum algorithm by

--- ALGORITHMS/javascript/twoSum.js
+++ ALGORITHMS/javascript/twoSum.js
@@ -2,6 +2,8 @@ function twoNumberSum(array, targetSum) {
   // Write your code here.
 	// targetSum = current + x(anynumber)
 	// x = targetSum - current  
+    // time complexity: O(n^2)
+    // space complexity: O(1)
 	let i
 	let j
 	for (i = 0; i < array.length; i++) {
