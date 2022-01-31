0e5394d491f2c59b79603090b36966acd4ac42de
Ewooral <boahen.cos@gmail.com>
2022-01-31 17:54:16 +0000

    two_sum first solution using two for loops (time and space complexity) of o(n^2)

--- ALGORITHMS/python/two_sum.py
+++ ALGORITHMS/python/two_sum.py
@@ -10,3 +10,16 @@ array = [3, 5, -4, 8, 11, 1, -1, 6]
 targetSum = 10
 
 '''
+
+def two_number_sum(array, targetSum):
+    # Write your code here.
+    # O(n^2)
+    for i in range(len(array)):
+        for j in range(i+1, len(array)):
+            if array[i] + array[j] == targetSum:
+                return [array[i], array[j]]
+    return []
+
+
+print(two_number_sum([3, 5, -4, 8, 11, 1, -1, 6], 10))
+print(two_number_sum([4,6,1,-3], 3))
