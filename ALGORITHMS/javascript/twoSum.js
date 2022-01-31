c0107bd238663ebe13739a18c729dca573eed78d
Ewooral <boahen.cos@gmail.com>
2022-01-31 21:41:24 +0000

    Two sum algorithm in JavaScript

---
+++ ALGORITHMS/javascript/twoSum.js
@@ -0,0 +1,19 @@
+function twoNumberSum(array, targetSum) {
+  // Write your code here.
+	// targetSum = current + x(anynumber)
+	// x = targetSum - current  
+	let i
+	let j
+	for (i = 0; i < array.length; i++) {
+		 for(j = i+1; j < array.length; j++) {
+			 if(array[i] + array[j] == targetSum){
+				 return [array[i], array[j]]
+			 }
+			 
+		 }
+		
+	}
+	return []
+	
+}
+console.log(twoNumberSum([-21, 301, 12, 4, 65, 56, 210, 356, 9, -47], 163));
