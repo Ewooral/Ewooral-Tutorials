00ae39ae8fa7df1898fcce8de88ef3fcbc02c4d1
Ewooral <boahen.cos@gmail.com>
2022-02-05 02:58:10 +0000

    java tut

--- Java Projects/HelloWorld.java
+++ Java Projects/HelloWorld.java
@@ -1,12 +1,29 @@
-//public class HelloWorld{
-//    public static void main(String[] args){
-//        System.out.println("Hello, World! It's me Elijah");
-//    }
-//}
+import java.lang.Math;
+
 
     public class HelloWorld{
         public static void main(String[] args) {
-            System.out.println("Hey Bernice, Stop making noise");
+            // FLOATS
+           float ab = 0.0f; 
+           int a = 4;
+           int b = 5;
+           float sum = (float)a + b;   // it is imperative that you cast one of the operands to
+                                       //  float when dealing with floating point numbers
+         
+          int num = (int) Math.pow(a, 2);
+          System.out.println(num);
+
+            // DOUBLES
+            double c = 0.0;
+            String name = "Elijah";
+            float j = 3.3f;
+            double d = 4.0;
+            double e = 5.0;
+            float Sum2 = (float)(c + d + e);
+
+         
+         System.out.println( sum);
+         System.out.println( name);
         }
 
-    }
+    }   
