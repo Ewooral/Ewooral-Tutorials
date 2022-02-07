3b98c2179dcf6c5cf8a7e580ad8114741d44e2b2
Ewooral <boahen.cos@gmail.com>
2022-02-07 20:24:30 +0000

    three sum algorithm third approach

--- ALGORITHMS/python/three_number_sum.py
+++ ALGORITHMS/python/three_number_sum.py
@@ -52,27 +52,30 @@ print(threeNumberSum(12, 3, 1, 2, -6, 5, -8, 6),0)
 
 # APPROACH THREE 
 class Solution:
-    def threeSum(self, nums):
-        res = []
-        nums.sort()
-        for i in range(len(nums)):
-            if nums[i] > 0:
-                break
-            if i == 0 or nums[i - 1] != nums[i]:
-                self.twoSumII(nums, i, res)
-        return res
 
-    def twoSumII(self, nums, i, res):
-        lo, hi = i + 1, len(nums) - 1
-        while (lo < hi):
-            sum = nums[i] + nums[lo] + nums[hi]
-            if sum < 0:
-                lo += 1
-            elif sum > 0:
-                hi -= 1
-            else:
-                res.append([nums[i], nums[lo], nums[hi]])
-                lo += 1
-                hi -= 1
-                while lo < hi and nums[lo] == nums[lo - 1]:
-                    lo += 1
+
+# class Solution:
+#     def threeSum(self, nums):
+#         res = []
+#         nums.sort()
+#         for i in range(len(nums)):
+#             if nums[i] > 0:
+#                 break
+#             if i == 0 or nums[i - 1] != nums[i]:
+#                 self.twoSumII(nums, i, res)
+#         return res
+
+#     def twoSumII(self, nums, i, res):
+#         lo, hi = i + 1, len(nums) - 1
+#         while (lo < hi):
+#             sum = nums[i] + nums[lo] + nums[hi]
+#             if sum < 0:
+#                 lo += 1
+#             elif sum > 0:
+#                 hi -= 1
+#             else:
+#                 res.append([nums[i], nums[lo], nums[hi]])
+#                 lo += 1
+#                 hi -= 1
+#                 while lo < hi and nums[lo] == nums[lo - 1]:
+#                     lo += 1
