2e99d476022fbaaf97952b16e708884781f6dab8
Ewooral <boahen.cos@gmail.com>
2022-02-08 23:56:34 +0000

    three sum algorithm third approach

--- ALGORITHMS/python/three_number_sum.py
+++ ALGORITHMS/python/three_number_sum.py
@@ -113,3 +113,28 @@ Space Complexity: from O(logn) to O(n), depending on the
 implementation of the sorting algorithm. For the purpose of 
 complexity analysis, we ignore the memory required for the output.
 """
+
+
+class Solution:
+    def threeSum(self, nums):
+        res = []
+        nums.sort()
+        for i in range(len(nums)):
+            if nums[i] > 0:
+                break
+            if i == 0 or nums[i - 1] != nums[i]:
+                self.twoSumII(nums, i, res)
+        return res
+
+    def twoSumII(self, nums, i, res):
+        lo, hi = i + 1, len(nums) - 1
+        while (lo < hi):
+            sum = nums[i] + nums[lo] + nums[hi]
+            if sum < 0:
+                lo += 1
+            elif sum > 0:
+                hi -= 1
+            else:
+                res.append([nums[i], nums[lo], nums[hi]])
+                lo += 1
+                hi -= 1
