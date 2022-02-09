2611e3c459ef96c6691d193d6dad296bc0f7c7c5
Ewooral <boahen.cos@gmail.com>
2022-02-09 19:23:43 +0000

    remove duplicates from an array algorithm

--- ALGORITHMS/python/remove_duplicate.py
+++ ALGORITHMS/python/remove_duplicate.py
@@ -6,13 +6,20 @@ class Solution(object):
         """
         # Initialize the counter and the array index.
         i, count = 1, 1
+        container = []
         # Start from the second element of the array and process
         # elements one by one.
         while i < len(nums):
+
             # If the current element is a duplicate,
             # increment the count.
             if nums[i] == nums[i - 1]:
                 count += 1
+
                 # If the count is more than 2, this is an
                 # unwanted duplicate element and hence we
                 # remove it from the array.
+
+                if count > 2:
+                    nums.pop(i)
+                    container.append(i)
