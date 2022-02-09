7105272986cf88bb1664089960defea7d6758aa1
Ewooral <boahen.cos@gmail.com>
2022-02-09 19:06:31 +0000

    three sum algorithm third approach

--- ALGORITHMS/python/remove_duplicate.py
+++ ALGORITHMS/python/remove_duplicate.py
@@ -1,2 +1,8 @@
 class Solution(object):
-    def removeDuplicates(self, nums):
+    def remove_duplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Initialize the counter and the array index.
+        i, count = 1, 1
