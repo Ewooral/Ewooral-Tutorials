448e0aab76bf7571b13d0200787124f4c7b21898
Ewooral <boahen.cos@gmail.com>
2022-02-09 20:46:51 +0000

    remove duplicate algorithm - Java codes

--- ALGORITHMS/java/removeDuplicates.java
+++ ALGORITHMS/java/removeDuplicates.java
@@ -123,3 +123,7 @@ class SecondSolution {
             // For a count <= 2, we copy the element over thus
             // overwriting the element at index "j" in the array
             //
+
+            if (count <= 2) {
+                nums[j++] = nums[i];
+            }
