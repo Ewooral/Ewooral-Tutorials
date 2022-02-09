b05e2f2d69e1cc939b9060795bcd650e6cb16ffd
Ewooral <boahen.cos@gmail.com>
2022-02-09 20:17:47 +0000

    remove duplicate algorithm - Java codes

---
+++ ALGORITHMS/java/removeDuplicates.java
@@ -0,0 +1,9 @@
+class Solution {
+    
+    public int[] remElement(int[] arr, int index) {
+        
+        //
+        // Overwrite the element at the given index by 
+        // moving all the elements to the right of the
+        // index, one position to the left.
+        //
