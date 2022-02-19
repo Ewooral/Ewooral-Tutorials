1a5e830baf0f23bfecec5949043e6099204204ea
Ewooral <boahen.cos@gmail.com>
2022-02-19 13:24:38 +0000

    a function that accepts a string and  repeats it 5 times using recursion

--- PYTHON/oop/recursion.py
+++ PYTHON/oop/recursion.py
@@ -1,9 +1,13 @@
 counter = 0
 def recurse():
+    global counter # this is the global variable that counts the number of times the function is called 
+    if counter == 888: return "done!"
     counter += 1
     print(counter)
-    recurse()
+    return recurse()
 
-recurse()
 
+print(recurse())
+print(counter)
 # A base case in recursion stops the  loop at some point.
+# The global keyword is used to access the global variable. 
