b4d39483d6d5f84ce56b75c1795d5c46c3264b6d
Ewooral <boahen.cos@gmail.com>
2022-02-21 07:43:42 +0000

    call backs and higher order functions

--- JAVASCRIPT/callBacks.js
+++ JAVASCRIPT/callBacks.js
@@ -26,7 +26,7 @@ a function that takes at least one function as input and/or returns a function a
 
 const array = [1, 2, 3];
 
-// function update(callback) {
+function update(callback) {
 //   const output = [];
 
 //   for( up of array) {
