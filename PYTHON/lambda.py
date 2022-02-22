01aa0e2c0b2efd26874e6ced0b157a19b0730087
Ewooral <boahen.cos@gmail.com>
2022-02-22 15:36:38 +0000

    list comprehension in python

--- PYTHON/lambda.py
+++ PYTHON/lambda.py
@@ -1,6 +1,5 @@
 
 """
-
 A Lambda Function in Python programming
 is an anonymous function or a function having no name
 
@@ -12,6 +11,20 @@ you will use the lambda keyword
 SYNTAX:
 lambda p1, p2: expression
 
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+IIFE in Python Lambda
+IIFE stands for immediately invoked function execution. 
+It means that a lambda function is callable as soon as it is defined. 
+Let's understand this with an example; fire up your IDLE and type in
+the following:
+
+(lambda x: x + x)(2)
+
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+
 """
 
 # THIS 
@@ -20,15 +33,39 @@ def sum(a, b):
 print(sum(4, 4))
 
 # OR
-
 (lambda a, b: print(a + b))(2, 3)
 
+print((lambda x: x + x)(2))
 
 string = 'some kind of a useless lambda'
 (lambda y: print(y))(string)
 
 (lambda x: print(x))(string)
 
-# print(list(map(lambda x: x[:2], ['red', 'green', 'blue'])))
-# print(list(map(lambda x: x[::-1], ['red', 'green', 'blue'])))
-# print(list(map(lambda x: x[::], ['red', 'green', 'blue'])))
+print(list(map(lambda x: x[:2], ['red', 'green', 'blue'])))
+print(list(map(lambda x: x[::-1], ['red', 'green', 'blue'])))
+print(list(map(lambda x: x[::], ['red', 'green', 'blue'])))
+
+""" 
+lambdas in filter()
+
+The filter function is used to select some particular elements from a
+sequence of elements. The sequence can be any iterator like lists, sets,
+tuples, etc.
+
+The elements which will be selected is based on some pre-defined
+constraint. It takes 2 parameters:
+
+A function that defines the filtering constraint
+A sequence (any iterator like lists, tuples, etc.)
+"""
+sequences = [10, 2, 8, 7, 5, 4, 11, 0, 1] 
+
+filtered_sequences = filter(lambda x: x % 2 == 0, sequences)
+filtered_sequences_I = filter(lambda x: x > 4, sequences)
+print(list(filtered_sequences))
+print(list(filtered_sequences_I))
+
+
+
+
--- PYTHON/tempCodeRunnerFile.py
+++ PYTHON/tempCodeRunnerFile.py
@@ -1,4 +1,1 @@
-
-# print(list(map(lambda x: x[:2], ['red', 'green', 'blue'])))
-# print(list(map(lambda x: x[::-1], ['red', 'green', 'blue'])))
-# print(list(map(lambda x: x[::], ['red', 'green', 'blue'])))
+print(
