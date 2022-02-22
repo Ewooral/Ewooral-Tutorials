2894f2de8b328f175e3845563d1cf3ac71256e51
Ewooral <boahen.cos@gmail.com>
2022-02-22 14:08:27 +0000

    lamda keyword in python

--- PYTHON/lambda.py
+++ PYTHON/lambda.py
@@ -1,24 +1,34 @@
 
-"""A Lambda Function in Python programming
+"""
+
+A Lambda Function in Python programming
 is an anonymous function or a function having no name
 
 In Python, lambda expressions (or lambda forms) are
 utilized to construct anonymous functions. To do so, 
-you will use the lambda keyword (just as you use def to define
-normal functions)
+you will use the lambda keyword 
+(just as you use def to define normal functions)
 
 SYNTAX:
 lambda p1, p2: expression
+
 """
 
-sum = lambda a, b: a + b
-print("Result: ", sum(2, 3))
+# THIS 
+def sum(a, b):
+    return a + b
+print(sum(4, 4))
+
+# OR
+
+(lambda a, b: print(a + b))(2, 3)
+
 
 string = 'some kind of a useless lambda'
-print(lambda string: print(string))
+(lambda y: print(y))(string)
 
 (lambda x: print(x))(string)
 
-print(list(map(lambda x: x[:2], ['red', 'green', 'blue'])))
-print(list(map(lambda x: x[::-1], ['red', 'green', 'blue'])))
-print(list(map(lambda x: x[::], ['red', 'green', 'blue'])))
+# print(list(map(lambda x: x[:2], ['red', 'green', 'blue'])))
+# print(list(map(lambda x: x[::-1], ['red', 'green', 'blue'])))
+# print(list(map(lambda x: x[::], ['red', 'green', 'blue'])))
--- PYTHON/tempCodeRunnerFile.py
+++ PYTHON/tempCodeRunnerFile.py
@@ -1,29 +1,4 @@
-# Python3 program for the above approach
 
-# # Function to check if the weights
-# # can be delivered in D days or not
-# def isValid(weight, n, D, mx):
-
-#     # Stores the count of days required
-#     # to ship all the weights if the
-#     # maximum capacity is mx
-#     st = 1
-#     sum = 0
-
-#     # Traverse all the weights
-#     for i in range(n):
-#         sum += weight[i]
-
-#         # If total weight is more than
-#         # the maximum capacity
-#         if (sum > mx):
-#             st += 1
-#             sum = weight[i]
-
-#         # If days are more than D,
-#         # then return false
-#         if (st > D):
-#             return False
-
-#     # Return true for the days < D
-#     return True
+# print(list(map(lambda x: x[:2], ['red', 'green', 'blue'])))
+# print(list(map(lambda x: x[::-1], ['red', 'green', 'blue'])))
+# print(list(map(lambda x: x[::], ['red', 'green', 'blue'])))
