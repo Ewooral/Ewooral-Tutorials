752020b22ddb2d6ef5b49d3c71c5aa79ddffd90e
Ewooral <boahen.cos@gmail.com>
2022-02-24 10:58:03 +0000

    dimensional arrays in python

--- PYTHON/dimensional_arrays.py
+++ PYTHON/dimensional_arrays.py
@@ -11,8 +11,23 @@ print(arr)
 # Using the above first method to create a 2 D array
 rows, cols = (5, 5)
 arr = [[0]*cols]*rows
+#  a = [0] * cols // [0, 0, 0, 0, 0]
+# b = [a] * rows // [[a], [a], [a], [a], [a]]
 print(arr)
 
-rows1, cols2 = {0:"rows", 1:"cols"}
-print(rows)
-print(cols2)
+# Using above second method to create a
+# 2D array
+arr2 = [[0 for i in range(cols)] for j in range(rows)]
+print(arr2)
+
+
+arr3 = []
+for i in range(rows):
+    col = []
+    for j in range(cols):
+        col.append(0)
+    arr3.append(col)
+print(arr3)
+
+arr3[0][0] = 23
+print("array three: ", arr3[0][0] )
