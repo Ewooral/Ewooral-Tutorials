b107d5f68707092ef9dc5c6e416cd9e4918cbc76
Ewooral <boahen.cos@gmail.com>
2022-02-24 11:14:17 +0000

    dimensional arrays in python

--- PYTHON/dimensional_arrays.py
+++ PYTHON/dimensional_arrays.py
@@ -24,10 +24,14 @@ print(arr2)
 arr3 = []
 for i in range(rows):
     col = []
+    # print(col)
     for j in range(cols):
         col.append(0)
+    print("Let me see: ",col, len(col))
     arr3.append(col)
-print(arr3)
+print("col: ",arr3)
 
-arr3[0][0] = 23
-print("array three: ", arr3[0][0] )
+# arr3[0][1] = 23
+# for row in arr3[0]:
+#     print(row)
+
