995bce8b3247f78eeeb322263d7f98b705421f17
Ewooral <boahen.cos@gmail.com>
2022-03-08 17:32:52 +0000

    c and c++ essential concepts

--- C++/c and c++/introduction.cpp
+++ C++/c and c++/introduction.cpp
@@ -4,7 +4,16 @@ int main(){
     std:: cout << "Hello, world" << std:: endl; 
     
     /*This is how to write c++ comments*/
+    // Another way of writing a one line comment.
 
     return 0;
 }
 
+// ERRORS AND WARNINGS IN C++ 
+
+/*There are three types of warnings
+1. Compile Time Errors
+2. Runtime Errors
+3. Warnings
+
+*/
