600ef33678e1a6d646a32a8920311ed95337c382
Ewooral <boahen.cos@gmail.com>
2022-03-09 13:14:34 +0000

    c and c++ fundamentals

--- C++/c and c++/introduction.cpp
+++ C++/c and c++/introduction.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
 // DECLARATIONAL FUNCTIONS
-long addNumber(long first, long second); 
+
+long addNumber(long first, long second);
 int multiply(int huge, int small);
 int GetData();
+int getDataWithSpace();
 
     // MAIN FUNCTION
     int main()
@@ -11,27 +13,24 @@ int GetData();
 
     int firstNum{100};
     int secondNum{39};
-    
 
     int sum = firstNum + secondNum;
 
     std::cout << "sum: " << sum << std::endl;
 
+    // calling a function
+    getDataWithSpace();
     GetData();
-
-    std:: cout << "Hello, world" << std:: endl;
+    
     std::cout << addNumber(3 ^ 3, 10 ^ 4) << std::endl;
-    std:: cout << "The Product of two numbers is: "<< multiply(40, 20) << std::endl;
-
-   
+    std::cout << "The Product of two numbers is: " << multiply(40, 20) << std::endl;
 
 
-    std:: cerr << "Something happened! " << std::endl;
-    std:: clog << "Log message: Please try again. " << std::endl;
+    // Logging Errors Messages
+    std::cerr << "Something happened! " << std::endl;
+    std::clog << "Log message: Please try again. " << std::endl;
 
-
-
-    return 0; 
+    return 0;
 }
 
 /*This is how to write c++ comments */
@@ -48,46 +47,62 @@ int GetData();
 
 // runtime error
 // 7/0;
- 
 
-// FUNCTION 
-long addNumber(long first, long second){
- long sum = first + second; 
+// FUNCTION
+long addNumber(long first, long second)
+{
+    long sum = first + second;
 
- return sum;
+    return sum;
 }
 
-int multiply(int huge, int small){
+
+
+
+int multiply(int huge, int small)
+{
     int product = huge * small;
 
     return product;
 }
 
-// GETTING DATA INPUT FROM A USER 
-int GetData(){
 
-    // GRABBING DATA WITH NO SPACES
-    // int age;
-    // std:: string name;
-    // std::cout << "Please type your name: " << std:: endl;
-    // std::cin >> name;
-    // std::cout<<"Please type your age: " << std::endl;
-    // std::cin >> age;
-    // std::cout << "Your name is: " << name <<" and you are "<< age <<" years old!" << std::endl;
 
-    // GRABBING DATA WITH SPACES                             
-    std:: string vehicleData;
+
+int getDataWithSpace()
+{
+    std::string vehicleData;
     int numberplate;
+
+    // GRABBING DATA WITH SPACES
     std::cout << "Please type your vehicle information: " << std::endl;
     std::getline(std::cin, vehicleData);
-
-
     std::cout << "Please type your number-plate: " << std::endl;
     std::cin >> numberplate;
 
-    std::cout << "Your vehicle's data are: " << vehicleData << 
-    " and your vehicle's number plate is: " << numberplate << std::endl;
+    // OUTPUT
+    std::cout << "Your vehicle's data are: " << vehicleData << " and your vehicle's number plate is: " << numberplate << std::endl;
 
     return 0;
+}
 
+
+
+// GETTING DATA INPUT FROM A USER
+int GetData()
+{
+
+    // declaring variables
+    int age;
+    std::string name;
+
+    // GRABBING DATA WITH NO SPACES
+    std::cout << "Please type your name: " << std::endl;
+    std::cin >> name;
+    std::cout << "Please type your age: " << std::endl;
+    std::cin >> age;
+
+    // OUTPUT
+    std::cout << "Your name is: " << name << " and you are " << age << " years old!" << std::endl;
+    return 0;
 }
