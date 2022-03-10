e68b1f0eefb08a3c9b301c5e8cbbd812f4a02106
Ewooral <boahen.cos@gmail.com>
2022-03-10 08:58:14 +0000

    array in c language

--- HAVARD-C-LECTURES/lec.c
+++ HAVARD-C-LECTURES/lec.c
@@ -5,5 +5,14 @@ int main()
     printf("Hello World\n");
 
     int A[10] = {0, 1, 2, 3, 4, 5, 6};
-    printf("%d", A[0]);
+    printf("%d", A[5]);
+    printf("\nThe numbers are: \n");
+
+    for(int i = 0; i < 10; i++){
+        if( i == A[i]){
+            printf("\n");
+            printf("%d", A[i]);
+            printf("\n");
+        }
+    }
 }
