af741001893a0b14aabb6bc7c1d7b6d0b3a8beb4
Ewooral <boahen.cos@gmail.com>
2022-03-10 15:36:16 +0000

    python built in functions

--- HAVARD-C-LECTURES/static_dynamic_Arrays.c
+++ HAVARD-C-LECTURES/static_dynamic_Arrays.c
@@ -1,7 +1,19 @@
 # include <stdio.h>
+# include <stdlib.h>
 
 int main(){
 
-printf("static and dynamic arrays");
+printf("static and dynamic arrays \n");
+// stored in the stack section of the memory
+int A[5]; 
+// stored in the stack section of the memory
+int *P;
+// stored at the heap section
+ P = (int *)malloc(5 * sizeof(int));
 
+printf("%d \n", P);
+
+// delete an element in the heap section of the memory
+free(P);
+printf("%d", P);
 }
