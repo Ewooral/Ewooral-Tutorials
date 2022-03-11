0e8d90717a7acccbbd68120742310dc2c24c07bd
Ewooral <boahen.cos@gmail.com>
2022-03-11 08:34:01 +0000

    python slicing

---
+++ PYTHON/slicing.py
@@ -0,0 +1,12 @@
+""" 
+In slicing, the first element of a sequence has index "0". 
+The second element of a sequence has index "1".
+The ith element of a sequence has index i - 1.
+
+The indices of any sequence "elem" starts at position "0"
+and end at position "len(elem) - 1"
+
+"""
+
+elem = " 2, 3, d, a, 0, 3, 9 ";
+       # 0  1  2  3  4  5  6
