cebfb80f7fd79051c551fe0ad64b4dfaee4fe76f
Ewooral <boahen.cos@gmail.com>
2022-03-13 09:47:14 +0000

    structure in c++

--- C++/c and c++/structure.cpp
+++ C++/c and c++/structure.cpp
@@ -1,3 +1,4 @@
+# include <iostream>
 /*
  STRUCTURE
 
@@ -6,5 +7,25 @@
 
  It is used for defining user defined data types
 
+ The size of a structure is the total amount of memory consumed by all its members
+
 
 */
+struct Rectangle    // definition
+{
+    int length;
+    std:: breadth;
+};
+ 
+int main()
+{
+    struct Rectangle k; // declaration
+    struct Rectangle r{10, 5};  // declaration and initialization
+
+    r.length = 15;
+    r.breadth = "Hey"
+
+    std::cout << r.length << std::endl;
+    std::cout << r.breadth << std::endl;
+    std::cout << "The area of a rectangle is " << r.area * r.width << std::endl;
+}
