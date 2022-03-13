98fa130b2df85dba6e2af917217d6454db20bbdf
Ewooral <boahen.cos@gmail.com>
2022-03-13 12:17:55 +0000

    structure in c++

--- C++/c and c++/pointers.cpp
+++ C++/c and c++/pointers.cpp
@@ -2,7 +2,14 @@
 
 int main(){
 
+int a = 10; 
+int *p; // pointer
 
+p = &a; // stores the memory address of variable "a"
+q = a; // store s the value 10 assigned to variable "a"
+
+std::cout << p << std::endl;
+std::cout << p << std::endl;
 
     return 0;
 }
