0fb58c0b09bc88f2573e6fbb7d320d716a1e5c94
Ewooral <boahen.cos@gmail.com>
2022-03-13 11:47:13 +0000

    structure in c++

--- C++/c and c++/tempCodeRunnerFile.cpp
+++ C++/c and c++/tempCodeRunnerFile.cpp
@@ -1,7 +1,1 @@
- int age;
-    std:: string name;
-    std::cout << "Please type your name: " << std:: endl;
-    std::cin >> name;
-    std::cout<<"Please type your age: " << std::endl;
-    std::cin >> age;
-    std::cout << "Your name is: " << name <<" and you are "<< age <<" years old!" << std::endl;
+S
