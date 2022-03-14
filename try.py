3d5b7086d6b0dfbb226575299a5a7d961d98e947
Ewooral <boahen.cos@gmail.com>
2022-03-14 23:56:54 +0000

    reverse a string  without a built in function or method

--- try.py
+++ try.py
@@ -1,7 +1,7 @@
 def reverse(str):
     if not str or len(str) < 2 or type(str) != 'string': 
         return "hmm... that's not a string";
-#     reverse_words = [];
+    reverse_words = [];
 #     last = len(str) - 1
 #     for i in range(last, -1, -1):
 #         reverse_words.append(str[i])
