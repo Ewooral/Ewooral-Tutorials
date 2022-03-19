e9b0a3e5005c4009b0c4016d67dfe66f5f3295e4
Ewooral <boahen.cos@gmail.com>
2022-03-19 23:55:21 +0000

    testing codes

--- tests/try test.py
+++ tests/try test.py
@@ -70,7 +70,7 @@ def arrayOfProduct2(arr):
     finalProduct = [1] * len(arr)
 
 #     # [5, 1, 4, 2]
-#     lp = 1
+    lp = 1
 #     for index in range(0, len(arr)):
 #         finalProduct[index] *= lp
 #         lp *= arr[index]
