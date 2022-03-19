ec435644631f87cf7a6e5db5a93e87ed54c54fd4
Ewooral <boahen.cos@gmail.com>
2022-03-19 23:53:05 +0000

    testing codes

--- tests/try test.py
+++ tests/try test.py
@@ -39,7 +39,7 @@ print("array of Products 1:", array_of_product([5, 1, 4, 2]))
 
 # # O(n) Time | O(n) space
 def arrayOfProduct(arr):
-#     finalProduct,  cp_r, cp_l = [1] * len(arr), [1] * len(arr), [1] * len(arr)
+    finalProduct,  cp_r, cp_l = [1] * len(arr), [1] * len(arr), [1] * len(arr)
 #    # [5, 1, 4, 2]
 #     cp = 1
 #     for index in range(0, len(arr)):
