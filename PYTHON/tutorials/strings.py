b343cd4a6bc0e55d4baf7d50d1721842d6c2ad23
Ewooral <boahen.cos@gmail.com>
2022-03-20 23:15:12 +0000

    string formatting

--- PYTHON/tutorials/strings.py
+++ PYTHON/tutorials/strings.py
@@ -4,6 +4,10 @@ errno = 50159747054;
 
 # Old fashioned
 print('Hello %s' % name);
-# 
+
+# Here, I’m using the %x format specifier to convert an int value to a
+# string and to represent it as a hexadecimal number:
 print('%x' % errno);
 
+
+print('Hey %s, there is a 0x%x error!' % (name, errno))
