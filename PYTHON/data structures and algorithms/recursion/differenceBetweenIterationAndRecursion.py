50c7c6958241c62953edf43dbfc2c1c264728e15
Ewooral <boahen.cos@gmail.com>
2022-03-21 14:39:32 +0000

    how to write recursive functions in three steps

--- PYTHON/data structures and algorithms/recursion/differenceBetweenIterationAndRecursion.py
+++ PYTHON/data structures and algorithms/recursion/differenceBetweenIterationAndRecursion.py
@@ -23,10 +23,11 @@ print(factorial(1));
 # print(factorial(-10))
 
 def fibonacci(n):
+    # n should be greater than or equals to 0 and a positive integer
     assert n >= 0 and int(n) == n, 'Only positive integers are accepted'
     if n in [0, 1]:
         return n;
     else:
         return fibonacci(n-1) + fibonacci(n-2);
 
-print(fibonacci(-6));
+print(fibonacci(12));
