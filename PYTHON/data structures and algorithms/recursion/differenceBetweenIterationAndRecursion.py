6c4eba6a5f4b74f46aa873a024c7827b7f9616a8
Ewooral <boahen.cos@gmail.com>
2022-03-21 10:40:29 +0000

    how to write recursive functions in three steps

--- PYTHON/data structures and algorithms/recursion/differenceBetweenIterationAndRecursion.py
+++ PYTHON/data structures and algorithms/recursion/differenceBetweenIterationAndRecursion.py
@@ -14,7 +14,7 @@ print(powerOfTwo(4))
 def factorial(n):
     assert n >= 0 and int(n) == n, 'The number must be positive integer only!';
     if n in [0, 1]:
-#         return 1;
+        return 1;
 #     else: return n * factorial(n-1) 
 
 # print(factorial(4))
