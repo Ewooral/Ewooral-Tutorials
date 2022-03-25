f26e062b52bc1f60ca31b001084e268b8fa51246
Ewooral <boahen.cos@gmail.com>
2022-03-25 15:01:59 +0000

    list operations and functions"

--- PYTHON/tutorials/list_operations.py
+++ PYTHON/tutorials/list_operations.py
@@ -9,27 +9,64 @@ if __name__ == '__main__':
     numList.sort();
     print(numList);
 
+    print("...............................................");
+
+    # reversed()
+    numList.reverse();
+    print(numList);
+
+    print("...............................................");
+
+
     # convert a char string to a list of strings using the list method
     print(list(strings));
+
+    print("...............................................");
 
     # convert word strings into lists using the split method and a delimeter
     wordstrings_to_liststrings = strings.split(delimeter);
     print("list of strings: ", wordstrings_to_liststrings);
 
+    print("...............................................");
+
+    wordstrings_to_liststrings.reverse();
+    print("reversed liststrings: ", wordstrings_to_liststrings);
+
+    print("...............................................");
+
     # convert a list of strings back to a string.
     liststrings_to_wordstrings = delimeter.join(wordstrings_to_liststrings);
     print("Strings: ", liststrings_to_wordstrings);
 
+    print("...............................................");
+
     # Append()
     newList = ["Cashew", "Blueberry"];
-    newList.append(List);
+    newList.append("Oranges");
     print("New List: ", newList);
+
+    print("...............................................");
 
     # extend() and sorted()
     newList.extend(sorted(List));
     numList.extend(List);
-    print("num List: ", numList);
-    print("New List: ", newList);
+    print("extended List 1: ", numList);
+    print("extended sorted List 2: ", newList);
+
+    print("...............................................");
+
+
+    # enumerate()
+    for i, mylists in enumerate(newList):
+        print(i, mylists);
+
+    print("...............................................");
+
+    j = 0;
+    while j < len(newList):
+        print(j, newList[j]);
+        j += 1;
+
 
 
 
