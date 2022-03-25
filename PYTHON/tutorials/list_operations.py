b082b5075d1999938cd9651cd2ecc534a4a95429
Ewooral <boahen.cos@gmail.com>
2022-03-25 13:29:32 +0000

    list operations and functions"

--- PYTHON/tutorials/list_operations.py
+++ PYTHON/tutorials/list_operations.py
@@ -2,6 +2,13 @@ List = ["Mangoes", "Pawpaw", "Guava", "Apples"];
 strings = "Sampson^is^a^strong^man";
 delimeter = "^";
 
+# convert a char string to a list of strings using the list method
 print(list(strings));
 
-print(strings.split(delimeter));
+# convert word strings into lists using the split method and a delimeter
+wordstrings_to_liststrings = strings.split(delimeter)
+print(wordstrings_to_liststrings)
+
+
+
+print(delimeter.join(wordstrings_to_liststrings))
