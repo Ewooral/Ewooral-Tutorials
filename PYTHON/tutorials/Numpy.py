516fa8a241b8da342d1c2d9d8863862f934c0a8d
Ewooral <boahen.cos@gmail.com>
2022-03-27 02:39:39 +0100

    brushing up on functions in python

---
+++ PYTHON/tutorials/Numpy.py
@@ -0,0 +1,5 @@
+import numpy as np
+newme = np.array([1, 9, 2, 3, 4, 5, 6, 7])
+print(newme)
+bmi = newme/newme**2
+print(bmi)
--- PYTHON/tutorials/funtions.py
+++ PYTHON/tutorials/funtions.py
@@ -9,3 +9,38 @@ def increment_dictionary_values(d, i):
 d = {"a":1, "b":2, "c":3}
 result = increment_dictionary_values(d, 2)
 print(result)
+
+
+
+
+
+
+
+# DATACAMP TUTORIALS
+
+# Create lists first and second
+first = [11.25, 18.0, 20.0]
+second = [10.75, 9.50]
+
+# Paste together first and second: full
+full = first + second
+
+# Sort full in descending order: full_sorted
+full_sorted = sorted(full,  reverse=True)
+
+# Print out full_sorted
+print(full_sorted)  # [20.0, 18.0, 11.25, 10.75, 9.5]
+
+
+# string to experiment with: place
+place = "poolhouse"
+
+# Use upper() on place: place_up
+place_up = place.upper()
+
+# Print out place and place_up
+print(place_up)
+
+# Print out the number of o's in place
+print(place.count("o"))
+
