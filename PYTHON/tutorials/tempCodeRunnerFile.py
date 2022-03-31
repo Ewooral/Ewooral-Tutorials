a70973ddc515c86eab826d22fcf77c05eadcb925
Ewooral <boahen.cos@gmail.com>
2022-04-01 00:53:31 +0100

    pandas in data science

--- PYTHON/tutorials/tempCodeRunnerFile.py
+++ PYTHON/tutorials/tempCodeRunnerFile.py
@@ -1,6 +1,16 @@
-for _ in range(len(mytuple) - 1, -len(mytuple) - 1, -1):
-#     if _ == "thirsty":
-#         continue;
-#     else:
-#         _ = _ -1
-   
+
+# # Store pop as a numpy array: np_pop
+# np_pop = np.array(pop)
+
+# # Double np_pop
+# np_pop *= 2
+
+# # Update: set s argument to np_pop
+# plt.scatter(gdp_cap, life_exp, s = np_pop)
+
+# # Previous customizations
+# plt.xscale('log')
+# plt.xlabel('GDP per Capita [in USD]')
+# plt.ylabel('Life Expectancy [in years]')
+# plt.title('World Development in 2007')
+# plt.xticks([1000, 10000, 100000], ['1k', '10k', '100k'])
