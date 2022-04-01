b9f608c67721ec174e1b248157e788b8cca40637
Ewooral <boahen.cos@gmail.com>
2022-04-01 01:31:28 +0100

    csv to DataFrame

--- PYTHON/tutorials/pandas.py
+++ PYTHON/tutorials/pandas.py
@@ -31,19 +31,6 @@ Instructions
     Print out cars and see how beautiful it is.
 
 
-Dictionary to DataFrame (2)
-
-The Python code that solves the previous exercise is included in the script. Have you noticed that the row labels (i.e. the labels for the different observations) were automatically set to integers from 0 up to 6?
-
-To solve this a list row_labels has been created. You can use it to specify the row labels of the cars DataFrame. You do this by setting the index attribute of cars, that you can access as cars.index.
-Instructions
-100 XP
-
-    Hit Run Code to see that, indeed, the row labels are not correctly set.
-    Specify the row labels by setting cars.index equal to row_labels.
-    Print out cars again and check if the row labels are correct this time.
-
-
 """
 
 
@@ -65,6 +52,28 @@ my_dict = {
 # Build a DataFrame cars from my_dict: cars
 cars = pd.DataFrame(my_dict)
 
+# Print cars
+print(".......................................................")
+print(cars)
+
+print("\n")
+"""
+
+
+Dictionary to DataFrame (2)
+
+The Python code that solves the previous exercise is included in the script. Have you noticed that the row labels (i.e. the labels for the different observations) were automatically set to integers from 0 up to 6?
+
+To solve this a list row_labels has been created. You can use it to specify the row labels of the cars DataFrame. You do this by setting the index attribute of cars, that you can access as cars.index.
+Instructions
+100 XP
+
+    Hit Run Code to see that, indeed, the row labels are not correctly set.
+    Specify the row labels by setting cars.index equal to row_labels.
+    Print out cars again and check if the row labels are correct this time.
+
+"""
+
 # Definition of row_labels
 row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
 
@@ -72,4 +81,24 @@ row_labels = ['US', 'AUS', 'JPN', 'IN', 'RU', 'MOR', 'EG']
 cars.index = row_labels
 
 # Print cars
+print(".......................................................")
 print(cars)
+
+
+
+"""
+CSV to DataFrame (1)
+
+Putting data in a dictionary and then building a DataFrame works, but it's not very efficient. What if you're dealing with millions of observations? In those cases, the data is typically available as files with a regular structure. One of those file types is the CSV file, which is short for "comma-separated values".
+
+To import CSV data into Python as a Pandas DataFrame you can use read_csv().
+
+Let's explore this function with the same cars data from the previous exercises. This time, however, the data is available in a CSV file, named cars.csv. It is available in your current working directory, so the path to the file is simply 'cars.csv'.
+Instructions
+100 XP
+
+    To import CSV files you still need the pandas package: import it as pd.
+    Use pd.read_csv() to import cars.csv data as a DataFrame. Store this DataFrame as cars.
+    Print out cars. Does everything look OK?
+
+"""
