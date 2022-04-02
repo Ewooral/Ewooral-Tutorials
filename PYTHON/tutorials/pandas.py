b958c17ae2bff08849bab6a5d84b89111a4100bf
Ewooral <boahen.cos@gmail.com>
2022-04-02 03:10:21 +0100

    using square brackets to print out columns and rows in pandas python

--- PYTHON/tutorials/pandas.py
+++ PYTHON/tutorials/pandas.py
@@ -105,7 +105,7 @@ Instructions
 print(".......................................................")
 
 # Import the cars.csv data: cars
-cars = pd.read_csv('./cars.csv')
+# cars = pd.read_csv("./cars.csv")
 
 # Print out cars
 print(cars)
@@ -128,7 +128,68 @@ Instructions
 """
 print(".......................................................")
 # Fix import by including index_col
-cars = pd.read_csv('./cars.csv', index_col=0)
+# cars = pd.read_csv("./cars.csv")
 
 # Print out cars
 print(cars)
+
+
+
+"""
+Square Brackets (1)
+
+In the video, you saw that you can index and select Pandas DataFrames in many different ways. The simplest, but not the most powerful way, is to use square brackets.
+
+In the sample code, the same cars data is imported from a CSV files as a Pandas DataFrame. To select only the cars_per_cap column from cars, you can use:
+
+cars['cars_per_cap']
+cars[['cars_per_cap']]
+
+The single bracket version gives a Pandas Series, the double bracket version gives a Pandas DataFrame.
+Instructions
+100 XP
+
+    Use single square brackets to print out the country column of cars as a Pandas Series.
+    Use double square brackets to print out the country column of cars as a Pandas DataFrame.
+    Use double square brackets to print out a DataFrame with both the country and drives_right columns of cars, in this order.
+
+"""
+print(".......................................................")
+
+# Import cars data
+# cars = pd.read_csv("./cars.csv", index_col=0)
+
+# Print out country column as Pandas Series
+print(cars['country'])
+
+# Print out country column as Pandas DataFrame
+print(cars[['country']])
+
+# Print out DataFrame with country and drives_right columns
+print(cars[['country', 'drives_right']])
+
+"""
+
+
+Square Brackets (2)
+
+Square brackets can do more than just selecting columns. You can also use them to get rows, or observations, from a DataFrame. The following call selects the first five rows from the cars DataFrame:
+
+cars[0:5]
+
+The result is another DataFrame containing only the rows you specified.
+
+Pay attention: You can only select rows using square brackets if you specify a slice, like 0:4. Also, you're using the integer indexes of the rows here, not the row labels!
+Instructions
+100 XP
+
+    Select the first 3 observations from cars and print them out.
+    Select the fourth, fifth and sixth observation, corresponding to row indexes 3, 4 and 5, and print them out.
+
+"""
+print(".......................................................")
+# Print out first 3 observations
+print(cars[:4])
+
+# Print out fourth, fifth and sixth observation
+print(cars[4:7])
