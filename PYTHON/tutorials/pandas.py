ce9a12e21f638260db16663fbdf94a71ded42cfa
Ewooral <boahen.cos@gmail.com>
2022-04-02 04:12:33 +0100

    pandas accessing columns and rows of a table using brackets

--- PYTHON/tutorials/pandas.py
+++ PYTHON/tutorials/pandas.py
@@ -261,3 +261,24 @@ print(cars.loc['MOR', 'drives_right'])
 print(".......................................................")
 # Print sub-DataFrame
 print(cars.loc[['RU', 'MOR'], ['country', 'drives_right']])
+
+
+"""
+loc and iloc (3)
+
+It's also possible to select only columns with loc and iloc. In both cases, you simply put a slice going from beginning to end in front of the comma:
+
+cars.loc[:, 'country']
+cars.iloc[:, 1]
+
+cars.loc[:, ['country','drives_right']]
+cars.iloc[:, [1, 2]]
+
+Instructions
+100 XP
+
+    Print out the drives_right column as a Series using loc or iloc.
+    Print out the drives_right column as a DataFrame using loc or iloc.
+    Print out both the cars_per_cap and drives_right column as a DataFrame using loc or iloc.
+
+"""
