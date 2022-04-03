b49385dee92ac33f1969c85aca1281816a179463
Ewooral <boahen.cos@gmail.com>
2022-04-03 02:45:16 +0100

    logic, control flow and filtering in Pandas and Numpy

--- PYTHON/tutorials/pandas.py
+++ PYTHON/tutorials/pandas.py
@@ -525,3 +525,15 @@ Instructions
     Print out car_maniac to see if you got it right.
 
 """
+print("...........................................")
+
+# Create car_maniac: observations that have a cars_per_cap over 500
+cpc = cars["cars_per_cap"]
+many_cars = cpc > 500
+print(many_cars)
+
+car_maniac = cars[many_cars]
+
+print("...........................................")
+# Print car_maniac
+print(car_maniac)
