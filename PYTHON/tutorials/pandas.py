d14d5f161bbfcb2b9f4a72c93545298725eec8ab
Ewooral <boahen.cos@gmail.com>
2022-04-03 01:54:19 +0100

    logic, control flow and filtering in Pandas

--- PYTHON/tutorials/pandas.py
+++ PYTHON/tutorials/pandas.py
@@ -422,3 +422,19 @@ Instructions
         double the area of my_kitchen is smaller than triple the area of your_kitchen.
 
 """
+print("...........................................")
+# Define variables
+my_kitchen = 18.0
+your_kitchen = 14.0
+
+print("...........................................")
+# my_kitchen bigger than 10 and smaller than 18?
+print(my_kitchen > 10 and my_kitchen < 18)
+
+print("...........................................")
+# my_kitchen smaller than 14 or bigger than 17?
+print(my_kitchen < 14 or my_kitchen > 17)
+
+print("...........................................")
+# Double my_kitchen smaller than triple your_kitchen?
+print(2 * my_kitchen < 3 * your_kitchen)
