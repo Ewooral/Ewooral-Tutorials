9f3d45a8f30c8e6de9991653b6bb2330ab51357f
Ewooral <boahen.cos@gmail.com>
2022-04-04 01:15:03 +0100

    logic and control flow in Data Science using numpy and Pandas

--- PYTHON/tutorials/pandas.py
+++ PYTHON/tutorials/pandas.py
@@ -782,7 +782,26 @@ for lab, row in cars.iterrows():
 
 
 
+"""
+Add column (1)
 
+In the video, Hugo showed you how to add the length of the country names of the brics DataFrame in a new column:
+
+for lab, row in brics.iterrows() :
+    brics.loc[lab, "name_length"] = len(row["country"])
+
+You can do similar things on the cars DataFrame.
+Instructions
+100 XP
+
+    Use a for loop to add a new column, named COUNTRY, that contains a uppercase version of the country names in the "country" column. You can use the string method upper() for this.
+    To see if your code worked, print out cars. Don't indent this code, so that it's not part of the for loop.
+
+# hint
+In the body of the for loop, it seems that you're not properly updating
+the "COUNTRY" column of cars. If you use lab, row as iterator variables, 
+you can use cars.loc[lab, "COUNTRY"] = row["country"].upper() to do the proper updates.
+"""
 
 
 
