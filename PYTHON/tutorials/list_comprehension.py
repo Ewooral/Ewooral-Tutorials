fb182a5529c15fb10206458093524b3330bc97a3
Ewooral <boahen.cos@gmail.com>
2022-04-08 01:13:50 +0100

    pythonic ways of writing loops

--- PYTHON/tutorials/list_comprehension.py
+++ PYTHON/tutorials/list_comprehension.py
@@ -44,3 +44,29 @@ while i < len(names):
 
 Let's explore some more Pythonic ways of doing this.
 """
+names = ['Jerry', 'Kramer', 'Elaine', 'George', 'Newman']
+i = 0
+new_list = []
+while i < len(names):
+    if len(names[i]) >= 6:
+        new_list.append(names[i])
+    i += 1
+print(new_list)
+
+
+"""
+A more Pythonic approach would loop over the contents 
+of names, rather than using an index variable. Print better_list.
+""" 
+# Print the list created by looping over the contents of names
+better_list = []
+for name in names:
+    if len(name) >= 6:
+        better_list.append(name)
+print(better_list)
+
+"""
+The best Pythonic way of doing this is by using
+list comprehension. Print best_list.
+"""
+print([name for name in names if len(name) >= 6])
