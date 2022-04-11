7fb869d598ea4b2b6d42aac60b9c77cc7468ab2a
Ewooral <boahen.cos@gmail.com>
2022-04-11 04:26:05 +0100

    advanced python

--- PYTHON/tutorials/advanced_python.py
+++ PYTHON/tutorials/advanced_python.py
@@ -173,6 +173,9 @@ Let's welcome your guests!
 * Use range() to create a list of arrival times (10 through 50 incremented by 10). Create the list arrival_times by unpacking the range object.
 
 * You realize your clock is three minutes fast. Convert the arrival_times list into a numpy array (called arrival_times_np) and use NumPy broadcasting to subtract three minutes from each arrival time.
+
+* Use list comprehension with enumerate() to pair each guest in the names list to their updated arrival time in the new_times array. You'll need to use the index variable created from using enumerate() on new_times to index the names list.
+
 """
 # Create a list of arrival times
 arrival_times = [*range(10, 51, 10)]
@@ -185,3 +188,20 @@ arrival_times_np = np.array(arrival_times)
 new_times = arrival_times_np - 3
 
 print(new_times)
+
+# Use list comprehension and enumerate to pair guests to new times
+guest_arrivals = [(names[key], time) for key, time in enumerate(new_times)]
+
+print(guest_arrivals)
+
+# Use list comprehension and enumerate to pair guests to new times
+guest_arrivals = [(names[i], time) for i, time in enumerate(new_times)]
+
+# Map the welcome_guest function to each (guest,time) pair
+
+""" 
+welcome_map = map(welcome_guest, guest_arrivals)
+
+ guest_welcomes = [*welcome_map]
+ print(*guest_welcomes, sep='\n')
+"""
