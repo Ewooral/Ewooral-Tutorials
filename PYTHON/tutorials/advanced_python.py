beaf8c69480e9da96535bdb71fc96348f0bccba5
Ewooral <boahen.cos@gmail.com>
2022-04-11 03:55:38 +0100

    advanced python

--- PYTHON/tutorials/advanced_python.py
+++ PYTHON/tutorials/advanced_python.py
@@ -1,3 +1,4 @@
+import numpy as np
 """
 Built-in practice: range()
 
@@ -120,3 +121,40 @@ names_uppercase = [*names_map]
 
 # Print the list created above
 print(names_uppercase)
+
+"""
+Practice with NumPy arrays
+
+Let's practice slicing numpy arrays and using NumPy's broadcasting concept. Remember, broadcasting refers to a numpy array's ability to vectorize operations, so they are performed on all elements of an object at once.
+
+A two-dimensional numpy array has been loaded into your session (called nums) and printed into the console for your convenience. numpy has been imported into your session as np.
+Instructions 1/2
+50 XP
+
+    1
+    2
+
+    Print the second row of nums.
+    Print the items of nums that are greater than six.
+    Create nums_dbl that doubles each number in nums.
+    Replace the third column in nums with a new column that adds 1 to each item in the original column.
+
+"""
+numsi = [
+    [ 1,  2,  3, 10,  5],
+    [ 6,  7,  8, 15, 10]
+ ]
+nums = np.array(numsi)
+# Print second row of nums
+print(nums[1,: ])
+
+# Print all elements of nums that are greater than six
+print(nums[nums > 6])
+
+# Double every element of nums
+nums_dbl = nums * 2
+print(nums_dbl)
+
+# Replace the third column of nums
+nums[:, 2] = nums[:, 2] + 1
+print(nums)
