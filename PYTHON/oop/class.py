4d8205d5a92903516645653795c08866f88a9e8c
Ewooral <boahen.cos@gmail.com>
2022-04-14 18:13:42 +0100

    access modifiers in oop

--- PYTHON/oop/class.py
+++ PYTHON/oop/class.py
@@ -1,59 +1,27 @@
-from unicodedata import name
-
-
 class Employee:
-    ID = str(input("key in your ID: \n"))
-    Age = str(input("Key in your Age: \n"))
+    ID = None
+    Age = None
     Salary = "$40,000"
     Department = "Computer Science"
 
     try:
-        def department(self) -> None: return self.Department 
          
-        def __init__(self) -> None:
-            _id = str(input("key in your ID: \n"))
-            age = str(input("Key in your Age: \n"))
-
-        @staticmethod
-        def name(): return Employee.Salary + str(2)
-
-        @classmethod
-        def get_name(cls):
-            cls.Salary = Employee.name()
-            print("my AGE is: ", cls.Age)
-            print("my ID is: ", cls.ID)
-            print("My Salary is per month is: ", cls.Salary)
+        def __init__(self):
+            self.salary = Employee.Salary
+            self.dep = Employee.Department
+            return self.salary
 
             
-
-    except (AttributeError, TypeError, ValueError) as err:
+    except (TypeError, AttributeError, ValueError) as err:
         print(f"Ooops!... {err}")
 
 
-class Player:
-    teamName = 'Liverpool'  # class variables
-
-    def __init__(self, name):
-        self.name = name  # creating instance variables
-
-    @staticmethod
-    def demo():
-        print("I am a static method.")
-        play = Player("Jumah Sumaila")
-        print(play.name)
-
-
-p1 = Player('lol')
-p1.demo()
 
 
 
     
 
 
-Employee.get_name()
-# Employee.department()
-# newEmp = Employee()
-# newEmp.department()
-
+James = Employee()
+print(James.dep)
 
---
+++ PYTHON/oop/class_static_methods.py
@@ -0,0 +1,22 @@
+class Player:
+    teamName = 'Liverpool'  # class variables
+
+    def __init__(self, name):
+        self.name = name  # creating instance variables
+    
+    @classmethod
+    def getTeamName(cls):
+        return cls.teamName
+
+
+    @staticmethod
+    def demo():
+        print("I am a static method.")
+        play = Player("Jumah Sumaila")
+        print(play.name)
+
+
+print(Player.getTeamName())
+p1 = Player('lol')
+p1.demo()
+
