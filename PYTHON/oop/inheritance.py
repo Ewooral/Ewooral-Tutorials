cd7ac739dede0f813a76c78ff74bed993886a02b
Ewooral <boahen.cos@gmail.com>
2022-04-20 23:50:36 +0100

    inheritance in python - oop

--- PYTHON/oop/inheritance.py
+++ PYTHON/oop/inheritance.py
@@ -77,8 +77,8 @@ class Vehicle:  # defining the parent class
 
 
 class Car(Vehicle):  # defining the child class
-#     # defining display method in the child class
-#     def display(self):
+    # defining display method in the child class
+    def display(self):
 #         super().display()
 #         print("I am from the Car Class")
 
