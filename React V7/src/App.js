7ce528b857b4bd60b6b42076077e0487cec0ffdd
Ewooral <boahen.cos@gmail.com>
2022-04-22 21:58:53 +0100

    types of inheritance in OOP python

---
+++ React V7/src/App.js
@@ -0,0 +1,43 @@
+const Pet = (props) => {
+    return (
+        React.createElement("div", {}, [
+            React.createElement("h1", {}, props.name),
+            React.createElement("h2", {}, props.animal),
+            React.createElement("h2", {}, props.breed),
+        ])
+    )
+
+}
+
+
+
+
+
+
+const App = () => {
+    return React.createElement(
+        "div",
+        {},
+        React.createElement("h1", { id: "brand" }, "Adopt Me!"),
+        React.createElement(Pet, {
+            name: "Luna",
+            animal: "Dog",
+            breed: "Havanese"
+        }),
+        React.createElement(Pet, {
+            name: "Kuka",
+            animal: "Cat",
+            breed: "Barkohese"
+        }),
+        React.createElement(Pet, {
+            name: "Suaso",
+            animal: "Ant",
+            breed: "Soldier Ant"
+        }),
+
+
+    )
+}
+ReactDOM.render(React.createElement(App),
+    document.getElementById("root")
+)
