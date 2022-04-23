86bcebb01adcaaa9fff2b4d98b7df2ebc44afc58
Ewooral <boahen.cos@gmail.com>
2022-04-23 11:46:47 +0100

    challenge 1 and solution on inheritance OOP

--- PYTHON/oop/solution1(inheritance).py
+++ PYTHON/oop/solution1(inheritance).py
@@ -1,5 +1,27 @@
 class Account:
-    def __init(self):
-        self.title = None
-        self.balance = None 
+    def __init__(self, title=None, balance=0):
+        self.title = title
+        self.balance = balance 
+
+    def tit(self):
+        print(self.title,"\n", self.balance)
+
+
+class SavingsAccount(Account):
+    def __init__(self, title=None , balance=0, interestRate=0 ):
+        super().__init__(title, balance)
+        self.interestRate = interestRate
+
+    def intR(self):
+        print("Interest Rate: ", self.interestRate)
+
+    def Tit(self):
+        return self.tit()
         
+
+Acc = Account("Mark", "GH 5.00")
+Acc.tit()
+print("....................")
+Sa = SavingsAccount("Kwesi", "GH 40,000", "5%")
+Sa.intR()
+Sa.Tit()
