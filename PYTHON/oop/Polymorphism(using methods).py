e0e160367aa761f22bd02578f6e66c3d5523b26a
Ewooral <boahen.cos@gmail.com>
2022-04-24 23:55:05 +0100

    polymorphism operator overloading

--- PYTHON/oop/Polymorphism(using methods).py
+++ PYTHON/oop/Polymorphism(using methods).py
@@ -25,7 +25,7 @@ class Circle():
 
 shapes = [Rectangle(6, 10), Circle(7)]
 print("Sides of a rectangle are", str(shapes[0].sides))
-print("Area of rectangle is:", str(shapes[0].getArea()))
+# print("Area of rectangle is:", str(shapes[0].getArea()))
 
 print("Sides of a circle are", str(shapes[1].sides))
 print("Area of circle is:", str(shapes[1].getArea()))
