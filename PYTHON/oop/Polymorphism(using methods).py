5fbfd2c4cb0c2b7a1291ac5555a118ca155013b3
Ewooral <boahen.cos@gmail.com>
2022-04-24 23:52:38 +0100

    polymorphism operator overloading

--- PYTHON/oop/Polymorphism(using methods).py
+++ PYTHON/oop/Polymorphism(using methods).py
@@ -3,7 +3,7 @@ class Rectangle():
 
     # initializer
     def __init__(self, width=0, height=0):
-        # self.width = width
+        self.width = width
         self.height = height
         self.sides = 4
 
