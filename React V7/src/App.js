c837769837b687249c64e8239c2d778a49acebd7
Ewooral <boahen.cos@gmail.com>
2022-04-24 02:05:53 +0100

    React v7

--- React V7/src/App.js
+++ React V7/src/App.js
@@ -1,12 +1,7 @@
 // /* global React ReactDOM */
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
+import React from "react";
+import {render} from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement(
@@ -30,4 +25,5 @@ const App = () => {
     })
   );
 };
-ReactDOM.render(React.createElement(App), document.getElementById("root"));
+
+render(React.createElement(App), document.getElementById("root"));
