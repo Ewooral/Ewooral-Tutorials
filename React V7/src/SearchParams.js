cf74a28384dc76bdb2b144f85aa678297cc4b621
Ewooral <boahen.cos@gmail.com>
2022-04-25 02:46:47 +0100

    react hooks plugin added to eslint file

--- React V7/src/SearchParams.js
+++ React V7/src/SearchParams.js
@@ -1,8 +1,14 @@
 import { useState } from "react"
 
 
+
 const SearchParams = () => {
+
+    const ANIMALS = ["bird", "cat", "dog", "rabbit", "fish"];
     const [location, setLocation] = useState("")
+    const [Animal, setAnimal] = useState("")
+ 
+    
 
   return (
     <div className="search-params">
@@ -16,6 +22,34 @@ const SearchParams = () => {
                 placeholder="Location" 
                 onChange={ e => setLocation(e.target.value)} 
                 />
+            </label>
+            <label htmlFor="animal">
+              Animal 
+              <select 
+              name="animal" 
+              id="animal"
+              value={animal}
+              onChange={ e => {
+                setAnimal(e.target.value)
+                setBreed("")
+                }}
+
+              onBlur={ e => {
+                setAnimal(e.target.value)
+                setBreed("")
+                }}
+              >
+
+              <option />
+              {ANIMALS.map(animal =>{
+                return (
+                <option key={animal} value={animal}>
+                  {animal}
+                </option>
+                )
+                })}
+
+              </select>
             </label>
             <button>Submit</button>
         </form>
