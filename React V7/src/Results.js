072bb333a65152b4ccef760e0754e8c3010c0b49
Ewooral <boahen.cos@gmail.com>
2022-04-26 02:39:26 +0100

    react custom hooks

--- React V7/src/Results.js
+++ React V7/src/Results.js
@@ -1,9 +1,27 @@
 import Pet from "./Pet";
 
-const Results = ({pets}) => {
+const Results = ({ pets }) => {
     return (
         <div>
-            
+            {
+                !pets.length ? (
+                    <h1>No Pets Found!</h1>
+                ) : (
+                    pets.map((pet) => (
+
+                        <Pet
+                            name={pet.name}
+                            animal={pet.animal}
+                            breed={pet.breed}
+                            key={pet.id}
+
+                        />
+
+                    ))
+                )
+            }
         </div>
     )
 }
+
+export default Results;
