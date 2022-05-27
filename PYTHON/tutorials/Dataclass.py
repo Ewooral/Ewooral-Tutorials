79344ca23b2c4b8a9e248288a6f27723554ee7c8
Ewooral <boahen.cos@gmail.com>
2022-05-27 11:13:08 +0000

    python dataclasses

--- PYTHON/tutorials/Dataclass.py
+++ PYTHON/tutorials/Dataclass.py
@@ -8,4 +8,24 @@ class Family:
     address: chr
 
 
+@dataclass
+class InventoryItem:
+    """Class for keeping track of an item in inventory."""
+    name: str
+    unit_price: float
+    quantity_on_hand: int = 0
 
+    def total_cost(self) -> float:
+        return self.unit_price * self.quantity_on_hand
+
+
+'''
+will add, among other things, a __init__() that looks like:
+
+def __init__(self, name: str, unit_price: float, quantity_on_hand: int = 0):
+    self.name = name
+    self.unit_price = unit_price
+    self.quantity_on_hand = quantity_on_hand
+Note that this method is automatically added to the class: 
+it is not directly specified in the InventoryItem definition shown above.
+'''
--- PYTHON/tutorials/structural_pattern_matching.py
+++ PYTHON/tutorials/structural_pattern_matching.py
@@ -9,10 +9,24 @@ def run_command(command: str):
             print(f"Unknown command: {other!r}.")
 
 
+def run_command_v2(command: str):
+    match command.split():
+        case ["load", filename]:
+            print(f"Loading file: {filename}")
+        case ["save", filename]:
+            print(f"Saving to file: {filename}")
+        case ["quit" | "exit" | "bye", *rest]:
+            print("Quitting the program.")
+            quit()
+        case _:
+            print(f"Unknown command: {command!r}.")
+
+
 def main():
     while True:
         command = input("s ")
-        run_command(command)
+        # run_command(command)
+        run_command_v2(command)
 
 
 if __name__ == "__main__":
