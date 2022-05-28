c041e5f9822eaeefc8ac56512ae4483df555a96c
Ewooral <boahen.cos@gmail.com>
2022-05-28 18:58:04 +0000

    Operators and the math class

--- JavaProjects/OperatorAndMathClass.java
+++ JavaProjects/OperatorAndMathClass.java
@@ -14,7 +14,7 @@ public class OperatorAndMathClass{
         System.out.println("Multiplication: " + result3);
 
         double result4 = (double)intValue1 / intValue2;
-//         System.out.println("Division: " + result4);
+        System.out.println("Division: " + result4);
 
 //         double result5 = (double)intValue1 % intValue2;
 //         System.out.println("Remainder: " + result5);
