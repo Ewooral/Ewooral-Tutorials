1a4fc6a6bc7172ff5863fc4de9bd40f6f765b47f
Ewooral <boahen.cos@gmail.com>
2022-07-25 23:55:21 +0000

    Amazon wareHouse package shipment Question and Answer

--- Dream Companies/Amazon/Amazon WareHouse Queries.py
+++ Dream Companies/Amazon/Amazon WareHouse Queries.py
@@ -113,7 +113,7 @@ print(performWareHouseQuery(ware_house))
 print("....Group 3....")
 # insert
 print(ware_house.insert_in("BF7006246000"))
-print(ware_house.insert_in("KU7003346455"))
+# print(ware_house.insert_in("KU7003346455"))
 
 print("Ship 2")
 print(performWareHouseQuery(ware_house))
