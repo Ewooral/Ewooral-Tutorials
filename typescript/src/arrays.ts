4d399b4a5c9b514ba3ce19a1633da63c2916a8a2
Ewooral <boahen.cos@gmail.com>
2022-10-21 22:16:37 +0000

    feat:Add typescript enum

--- typescript/src/arrays.ts
+++ typescript/src/arrays.ts
@@ -1,22 +1,10 @@
-//ARRAY
-// const Cars : [
-//      Cars:{
-//         make:string;
-//         model:string;
-//         year:number;
-//     }[]
-//     {
-//         make: 'Ford'
-//         model: 'Fiesta'
-//         year: 2018
-//     }
-// ]
-
 let arr :number[] = [1,2,3,4,5];
-let arrr: Array<number> = [4,5,6,7,2]; 
+let newArray: Array<number> = [4,5,6,7,2];
 console.log(arr[3]);
-console.log(arrr);
+console.log(newArray);
 console.log(typeof(arr));
-console.log(typeof(arrr));
+console.log(typeof(newArray));
 
 
+let myPeople: string[] = ["elia", "kb", "david", "john", "emma"]
+console.log(myPeople[1])
---
+++ typescript/src/try.ts
@@ -0,0 +1,4 @@
+let a: number = 45;
+console.log(a)
+const b: number = 0o744;
+
