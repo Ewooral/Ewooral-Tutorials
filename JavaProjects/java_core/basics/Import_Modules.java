7e42af9bc9eca2f2ff40142948625e3b7c64c530
Ewooral <boahen.cos@gmail.com>
2022-12-02 20:13:28 +0000

    Chore: Add shift operator inJava

--- JavaProjects/java_core/basics/Import_Modules.java
+++ JavaProjects/java_core/basics/Import_Modules.java
@@ -1,15 +1,23 @@
 package basics;
 
-import Problem_Solving.CenteredArray;
-// import basics.VariableTypes1;
+import java.util.Arrays;
 
-public class VariableTypes {
+import Problem_Solving.CenteredArray;
+
+public class Import_Modules {
+    static String[] copyTo = { "a", "b", "c", "d", "e" };
+
     public static void main(String[] args) {
         int numbers1[] = { 3, 2, 1, 7, 5 };
         System.out.println(VariableTypes1.m);
         VariableTypes1.method();
         System.out.println(CenteredArray.middleElemen(numbers1));
         VariableTypes1.main(args);
+        HelloWorld.main(args);
+
+        Arrays.stream(copyTo)
+                .map(coffee -> coffee + ",")
+                .forEach(System.out::print);
     }
 
 }
