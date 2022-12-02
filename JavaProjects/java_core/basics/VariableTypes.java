92743ec8b02ed9fb5f4640b53f9a2325298f3367
Ewooral <boahen.cos@gmail.com>
2022-12-02 18:27:20 +0000

    ...

---
+++ JavaProjects/java_core/basics/VariableTypes.java
@@ -0,0 +1,15 @@
+package basics;
+
+import Problem_Solving.CenteredArray;
+// import basics.VariableTypes1;
+
+public class VariableTypes {
+    public static void main(String[] args) {
+        int numbers1[] = { 3, 2, 1, 7, 5 };
+        System.out.println(VariableTypes1.m);
+        VariableTypes1.method();
+        System.out.println(CenteredArray.middleElemen(numbers1));
+        VariableTypes1.main(args);
+    }
+
+}
