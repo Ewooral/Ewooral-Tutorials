15f2611fdd71597bb8d826c1f4bc10efac65f58b
Ewooral <boahen.cos@gmail.com>
2022-12-18 11:34:44 +0000

    chore: Add function raisetopower java lang

---
+++ JavaProjects/java_core/Problem_Solving/RaisedToPower.java
@@ -0,0 +1,15 @@
+public class RaisedToPower {
+    public static void main(String[] args) {
+        System.out.println(Math.pow(2, 7));
+        System.out.println(powOf(2, 7));
+    }
+
+    public static int powOf(int base, int pow) {
+        int res = 1;
+        int expo;
+        for (expo = pow; expo != 0; expo--) {
+            res *= base;
+        }
+        return res;
+    }
+}
