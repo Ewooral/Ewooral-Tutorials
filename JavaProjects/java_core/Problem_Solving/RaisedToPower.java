1c290df2ee65f07e08fce8d636debdc7206d8253
Ewooral <boahen.cos@gmail.com>
2022-12-19 02:15:42 +0000

    ...

--- JavaProjects/java_core/Problem_Solving/RaisedToPower.java
+++ JavaProjects/java_core/Problem_Solving/RaisedToPower.java
@@ -2,8 +2,8 @@ package Problem_Solving;
 
 public class RaisedToPower {
     public static void main(String[] args) {
-        System.out.println(Math.pow(2, 7));
-        System.out.println(powOf(2, 7));
+        System.out.println(Math.pow(2, -7));
+        System.out.println(powOf(2, -7));
     }
 
     public static int powOf(int base, int pow) {
@@ -12,6 +12,6 @@ public class RaisedToPower {
         for (expo = pow; expo != 0; expo--) {
             res *= base;
         }
-        return res;
+        return res > 0 ? res : 4;
     }
 }
