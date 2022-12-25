274b6d8f8ca23c59132884623baf5bcc9577612b
Ewooral <boahen.cos@gmail.com>
2022-12-25 23:38:31 +0000

    ...

--- JavaProjects/java_core/Problem_Solving/Str_CharacterFrequency.java
+++ JavaProjects/java_core/Problem_Solving/Str_CharacterFrequency.java
@@ -8,24 +8,24 @@ public class Str_CharacterFrequency {
     public static void frequency(String message) {
         int[] arr = new int[30];
         String visited = "";
-        for (int i = 0; i < message.length(); i++) {
-            String current = message.substring(i, i + 1);
-            if (visited.contains(current) || current.equals(" ")) {
-                continue;
-            }
-            int count = 1;
-            arr[i] = count;
-            for (int j = i + 1; j < message.length(); j++) {
-                String next = message.substring(j, j + 1);
-                if (current.equals(next)) {
-                    arr[i] = count + 1;
-                    count = arr[i];
-                }
-            }
-            System.out.print(current + ": ");
-            System.out.println(arr[i]);
-            visited += current;
+        // for (int i = 0; i < message.length(); i++) {
+        String current = message.substring(i, i + 1);
+        if (visited.contains(current) || current.equals(" ")) {
+            continue;
         }
+        int count = 1;
+        arr[i] = count;
+        for (int j = i + 1; j < message.length(); j++) {
+            String next = message.substring(j, j + 1);
+            if (current.equals(next)) {
+                arr[i] = count + 1;
+                count = arr[i];
+            }
+        }
+        System.out.print(current + ": ");
+        System.out.println(arr[i]);
+        visited += current;
+    }
 
     }
 
