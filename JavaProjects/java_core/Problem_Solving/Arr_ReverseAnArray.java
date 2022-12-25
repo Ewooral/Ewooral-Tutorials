63b53c78a852df1ca9beba2427de15b7141298a5
Ewooral <boahen.cos@gmail.com>
2022-12-25 23:11:11 +0000

    ...

--- JavaProjects/java_core/Problem_Solving/Arr_ReverseAnArray.java
+++ JavaProjects/java_core/Problem_Solving/Arr_ReverseAnArray.java
@@ -28,7 +28,7 @@ public class Arr_ReverseAnArray {
 
     // using Collection.reverse()
     public void reverse(Integer a[]) {
-        Collections.reverse(Arrays.asList(a));
+        // Collections.reverse(Arrays.asList(a));
         System.out.println(Arrays.asList(a));
 
     }
