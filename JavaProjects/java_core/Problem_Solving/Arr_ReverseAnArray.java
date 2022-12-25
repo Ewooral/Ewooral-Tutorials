c69d519a85e12a093e06339103ffafc3b2a6aa6c
Ewooral <boahen.cos@gmail.com>
2022-12-25 23:08:08 +0000

    ...

--- JavaProjects/java_core/Problem_Solving/Arr_ReverseAnArray.java
+++ JavaProjects/java_core/Problem_Solving/Arr_ReverseAnArray.java
@@ -2,36 +2,27 @@ package Problem_Solving;
 
 import java.util.*;
 
-public class Arr_ReverseAnArray {
-    /**
-     * Given an array, the task is to reverse the given array in Java.
-     * 
-     * Examples: 
-     * 
-     * Input : 1, 2, 3, 4, 5
-     * Output :5, 4, 3, 2, 1
-     * 
-     * Input : 10, 20, 30, 40
-     * Output : 40, 30, 20, 10
-     */
+// public class Arr_ReverseAnArray {
+/**
+ * Given an array, the task is to reverse the given array in Java.
+ * 
+ * Examples: 
+ * 
+ * Input : 1, 2, 3, 4, 5
+ * Output :5, 4, 3, 2, 1
+ * 
+ * Input : 10, 20, 30, 40
+ * Output : 40, 30, 20, 10
+ */
 
-    ArrayList<Integer> reversed = new ArrayList<Integer>();
+ArrayList<Integer>reversed=new ArrayList<Integer>();
 
-    public ArrayList<Integer> reverseArray(Integer[] arr) {
-        for (int i = arr.length - 1; i != -1; i--) {
-            int lastElem = arr[i];
-            reversed.add(lastElem);
-        }
-        return reversed;
-    }
+public ArrayList<Integer>reverseArray(Integer[]arr){for(int i=arr.length-1;i!=-1;i--){int lastElem=arr[i];reversed.add(lastElem);}return reversed;}
 
-    // using Collection.reverse()
-    public void reverse(Integer a[]) {
-        Collections.reverse(Arrays.asList(a));
-        System.out.println(Arrays.asList(a));
+// using Collection.reverse()
+public void reverse(Integer a[]){Collections.reverse(Arrays.asList(a));System.out.println(Arrays.asList(a));
 
-    }
-}
+}}
 
 class Test {
     public static void main(String[] args) {
