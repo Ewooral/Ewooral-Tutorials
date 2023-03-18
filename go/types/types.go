f554492d6ae8f47f736e3a71956ae77e86687531
Ewooral <boahen.cos@gmail.com>
2023-03-18 23:55:56 +0000

    chore: Types in Go language

--- go/types/types.go
+++ go/types/types.go
@@ -2,10 +2,11 @@ package main
 
 import "fmt"
 
-func name() {
+func name() int {
 	var name string = "Elijah"
 	var age int
 	age = 30
 	fmt.Println("My name is: ", name)
 	fmt.Println("I am", age, "years old!")
+	return age
 }
