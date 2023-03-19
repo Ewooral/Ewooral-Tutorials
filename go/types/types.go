15b45cff95982ca97b49487f3f9c444a63457246
Ewooral <boahen.cos@gmail.com>
2023-03-19 00:23:30 +0000

    chore: Scope in Go language

--- go/types/types.go
+++ go/types/types.go
@@ -2,11 +2,16 @@ package main
 
 import "fmt"
 
+// The variable 'Test' is available both inside and
+// outside the scope of this module
+var Test string = "Scope in GoLang"
+
 func name() int {
 	var name string = "Elijah"
 	var age int
 	age = 30
 	fmt.Println("My name is: ", name)
 	fmt.Println("I am", age, "years old!")
+	fmt.Println(`Hey people `)
 	return age
 }
