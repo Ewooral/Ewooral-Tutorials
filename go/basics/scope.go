08af7fa3197b8806008354c5bddb03159f7cf1d5
Ewooral <boahen.cos@gmail.com>
2023-03-19 17:30:59 +0000

    chore: constant in Go language

--- go/basics/scope.go
+++ go/basics/scope.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"rsc.io/quote"
+)
 
 var x int = 5
 
@@ -8,7 +12,14 @@ func scope() {
 	x = 7
 	var y int = 8
 
+	fmt.Println(quote.Go())
+	fmt.Println(quote.Hello())
+	fmt.Println(quote.Glass())
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[1])
+
 	fmt.Println(x + y)
 	fmt.Println(Test)
+	fmt.Println(NAME_OF_LANGUAGE)
 
 }
