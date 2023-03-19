4266b25fd7384352672064da68cd12529a56c558
Ewooral <boahen.cos@gmail.com>
2023-03-19 00:56:22 +0000

    chore: constant in Go language

--- go/basics/variables_and_types.go
+++ go/basics/variables_and_types.go
@@ -8,12 +8,28 @@ var Test string = "Scope in GoLang"
 
 const NAME_OF_LANGUAGE = "GOLANG"
 
-func name() int {
+var Age int = 40
+
+// Define multiple variables
+var (
+	a = 5
+	b = 6
+	c = 7
+)
+
+func name() {
 	var name string = "Elijah"
-	var age int
-	age = 30
+
+	Age = 30
 	fmt.Println("My name is: ", name)
-	fmt.Println("I am", age, "years old!")
+	fmt.Println("I am", Age, "years old!")
 	fmt.Println(`Hey people `)
-	return age
+
+	v := "Hello, World"
+	fmt.Println(v, a, b, c)
+}
+
+func age() {
+	Age = 80
+	fmt.Println("AGE:", Age)
 }
