3ecf01de0f0fc5abd721768b95ee110a9352abbe
Ewooral <boahen.cos@gmail.com>
2023-03-25 17:04:48 +0000

    chore: Elixir tutorials

--- Elixir-and-Erlang/basics/strings.exs
+++ Elixir-and-Erlang/basics/strings.exs
@@ -8,3 +8,7 @@ IO.puts(firstname <> " " <> lastname)
 # Interpolation - placing a value in the middle of a string
 noun = "name of a person, \"animals\", place or a thing"
 IO.puts("Definition of a noun: #{noun}")
+
+age = 30
+address = "Ashaiman"
+IO.puts("Age: #{30} and Address: #{address}")
