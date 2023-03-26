91ca3b19c274b2c09506c641ad0483635f69f313
Ewooral <boahen.cos@gmail.com>
2023-03-26 05:38:50 +0000

    feat: Create anonymous function in elixir language

--- Elixir-and-Erlang/basics/anonymous_function.exs
+++ Elixir-and-Erlang/basics/anonymous_function.exs
@@ -1,4 +1,4 @@
-defmodule Cooment do
+defmodule Anonymous_Function do
   @moduledoc """
 
   Anonymous functions
@@ -10,23 +10,23 @@ defmodule Cooment do
   iex> add = fn a, b -> a + b end
 
   """
+
+  add = fn a, b -> a + b end
+  IO.puts(add.(10, 20))
+  # Note that a dot (.) between the variable and parentheses
+  # is required to invoke an anonymous function
+
+  substract = fn c, d -> c - d end
+  IO.puts(substract.(7, 5))
+
+  # anonymous functions can also access variables that are in scope when the function is defined
+  double = fn a -> add.(a, a) end
+  IO.puts(double.(50))
+
+  # A variable assigned inside a function does not affect its surrounding environment:
+  x = 45
+  IO.puts((fn -> _x = 0 end).())
+  IO.puts(x)
+
+  IO.puts(length([2, 3, 6]))
 end
-
-add = fn a, b -> a + b end
-IO.puts(add.(10, 20))
-# Note that a dot (.) between the variable and parentheses
-# is required to invoke an anonymous function
-
-substract = fn c, d -> c - d end
-IO.puts(substract.(7, 5))
-
-# anonymous functions can also access variables that are in scope when the function is defined
-double = fn a -> add.(a, a) end
-IO.puts(double.(50))
-
-# A variable assigned inside a function does not affect its surrounding environment:
-x = 45
-IO.puts((fn -> x = 0 end).())
-IO.puts(x)
-
-IO.puts(length([2, 3, 6]))
