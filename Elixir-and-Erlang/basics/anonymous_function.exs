22f509b0c73d418867acf1dc329e0caa6ab61385
Ewooral <boahen.cos@gmail.com>
2023-03-26 05:11:00 +0000

    feat: Create anonymous function in elixir language

--- Elixir-and-Erlang/basics/anonymous_function.exs
+++ Elixir-and-Erlang/basics/anonymous_function.exs
@@ -1,5 +1,17 @@
+@doc """
+Anonymous functions
+
+Elixir also provides anonymous functions. Anonymous functions allow us to
+store and pass executable code around as if it was an integer or a string.
+They are delimited by the keywords fn and end:
+
+iex> add = fn a, b -> a + b end
+"""
+
 add = fn a, b -> a + b end
 IO.puts(add.(10, 20))
+# Note that a dot (.) between the variable and parentheses
+# is required to invoke an anonymous function
 
 substract = fn c, d -> c - d end
 IO.puts(substract.(7, 5))
