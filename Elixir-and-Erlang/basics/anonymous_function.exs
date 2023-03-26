78de11c30d7eeac0b17bc55c8b0f13c13408220a
Ewooral <boahen.cos@gmail.com>
2023-03-26 05:05:28 +0000

    feat: Create anonymous function in elixir language

--- Elixir-and-Erlang/basics/anonymous_function.exs
+++ Elixir-and-Erlang/basics/anonymous_function.exs
@@ -1,3 +1,9 @@
 add = fn a, b -> a + b end
-
 IO.puts(add.(10, 20))
+
+substract = fn c, d -> c - d end
+IO.puts(substract.(7, 5))
+
+# anonymous functions can also access variables that are in scope when the function is defined
+double = fn a -> add.(a, a) end
+IO.puts(double.(50))
