8c37f1dc9678cfc9ea69129b4039050a2949466c
Ewooral <boahen.cos@gmail.com>
2023-03-30 12:12:16 +0000

    chore: Concatenate two lists in Elixir

--- Elixir-and-Erlang/basics/Linked_list.exs
+++ Elixir-and-Erlang/basics/Linked_list.exs
@@ -1,4 +1,4 @@
 IO.puts("This is a linked list")
 concatenatate = [1, 2, 3] ++ [4, 5, 6]
-IO.puts(hd(concatenatate))
-IO.puts(tl(concatenatate))
+# IO.puts(hd(concatenatate))
+IO.puts(tl([4, 5, 6]))
