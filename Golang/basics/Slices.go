7f5cd6d8d4902dea243d721aa4c9849650f2bef8
Ewooral <boahen.cos@gmail.com>
2023-04-03 12:56:19 +0000

    Modify Slices.go module:

--- Golang/basics/Slices.go
+++ Golang/basics/Slices.go
@@ -69,11 +69,12 @@ func slices() {
 		  fmt.Println(slice1, slice2)
 		}
 	*/
-	fmt.Println("Slices functions, 'append' and 'copy' ")
+	fmt.Println("Slices functions, 'append' ")
 	slice1 := []int{1, 2, 3}
 	slice2 := append(slice1, 4, 5)
 	fmt.Println(slice1, slice2)
 
+	fmt.Println("Slices functions 'copy' ")
 	slice3 := []int{1, 2, 3}
 	slice4 := make([]int, 2)
 	copy(slice3, slice4)
