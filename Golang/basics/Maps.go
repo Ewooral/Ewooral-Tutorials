8b908c60918eed497578097c8913f920f4e1f56a
Ewooral <boahen.cos@gmail.com>
2023-04-06 22:52:01 +0000

    chore: Maps in golang

--- Golang/basics/Maps.go
+++ Golang/basics/Maps.go
@@ -1,10 +1,12 @@
 package main
 
-import "fmt"
-
 func maps() {
-	var x map[string]int
+	// var x map[string]int
+	// x["key"] = 10
+	// fmt.Println(x["key"])
+
+	x := make(map[string]int)
 	x["key"] = 10
-	fmt.Println(x["key"])
+	// fmt.Println(x["key"])
 
 }
