5c46472778d8c282f6c4b553ff4d0277d9362da9
Ewooral <boahen.cos@gmail.com>
2023-04-11 19:53:06 +0000

    chore: Maps in golang

--- Golang/basics/Maps.go
+++ Golang/basics/Maps.go
@@ -18,6 +18,15 @@ func useMap() {
 	elements := make(map[string]string)
 	elements["H"] = "Hydrogen"
 	elements["He"] = "Helium"
+	elements["Li"] = "Lithium"
+	elements["Be"] = "Beryllium"
+	elements["B"] = "Boron"
+	elements["C"] = "Carbon"
+	elements["N"] = "Nitrogen"
+	elements["O"] = "Oxygen"
+	elements["F"] = "Fluorine"
+	elements["Ne"] = "Neon"
 
+	fmt.Println(elements["Li"])
 	fmt.Println(elements)
 }
