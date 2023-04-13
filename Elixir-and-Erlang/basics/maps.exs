ac610da36852c396eaee0a99872851ca0571228f
Ewooral <boahen.cos@gmail.com>
2023-04-13 11:39:12 +0000

    feat: Use operators in elixir

--- Elixir-and-Erlang/basics/maps.exs
+++ Elixir-and-Erlang/basics/maps.exs
@@ -1,2 +1,11 @@
-employee = %{"first name" => "Eli", "last name" => ""}
+# create a map
+employee = %{"first name" => "Elijah", "last name" => "Boahen"}
 IO.puts(employee["first name"])
+
+# update
+john = %{employee | "first name" => "Samuel", "last name" => "Steele"}
+IO.puts(john["first name"])
+
+# using atoms as keys on maps
+letters = %{:a => "A", :b => "B", :c => "C", :d => "D"}
+IO.puts(letters.a)
