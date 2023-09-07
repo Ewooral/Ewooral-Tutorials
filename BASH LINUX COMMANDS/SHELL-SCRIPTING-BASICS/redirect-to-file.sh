eb7ad9b7bcd940298013a9ef6ef5c241ef734567
Ewooral <boahen.cos@gmail.com>
2023-09-07 14:32:41 +0000

    chore:Bash scripting fundamentals

---
+++ BASH LINUX COMMANDS/SHELL-SCRIPTING-BASICS/redirect-to-file.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+echo "I am being redirected to the new-script file"  > ./new-script.txt
+# only one greater than sign overwrites the file's contents
+
+
+# save the input of a user into a file
+cat > new-file.txt
+
+## append to a file
+
+cat >> new-file.txt
