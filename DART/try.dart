62e0a46c6e260814cb92399181cbbac49f7b2ec5
Ewooral <boahen.cos@gmail.com>
2024-07-27 23:19:26 +0000

    brush up on input and output, types, variable in dart

--- DART/try.dart
+++ DART/try.dart
@@ -1,17 +1,21 @@
-import 'dart:io';
+import 'variable/lib/variable.dart' as variable_func;
+import 'types/lib/types.dart' as types_func;
+import 'input-output/lib/input-output.dart' as input_output_func;
 
 main() {
-  print('hello, world');
-  var firstname = 'elijah';                                                                                                                                                                                                                                               
-  String lastname = 'boahen';
-  print(firstname + ' ' + lastname);
-  bool cow = true;
-  var length = 23;
-  var breadth = 2;
-  var height = 4;
-  var sum = length + breadth + height;
-  print(sum);
-  stdout.writeln('key in a number');
-  var num = stdin.readLineSync();
-  print('The number you entered is $num, $cow');
+  print('......VARIABLE........');
+  print('export variable from another file ${variable_func.x}');
+  print('the string function that' +
+      'returns the value of the variables is ${variable_func.variables()}');
+
+  print('......TYPES........');
+  print(types_func.str_to_num);
+  print(types_func.y_to_string);
+  print(types_func.letters_that_have_no_i_and_e_in_them);
+  print(types_func.name);
+  print(types_func.x);
+  print(types_func.sums);
+
+  print('......INPUT-OUTPUT........');
+  print(input_output_func.input_and_output());
 }
